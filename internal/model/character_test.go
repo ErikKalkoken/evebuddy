2804f90fe6d93f85d86bc0ddc93b7808dc35db2a
ErikKalkoken <erik.kalkoken@gmail.com>
2024-03-27 16:27:27 +0100

    Refactor character model tests

--- internal/model/character_test.go
+++ internal/model/character_test.go
@@ -39,77 +39,75 @@ func createCharacter(args ...model.Character) model.Character {
 	return c
 }
 
-func TestCharacterCanSaveNew(t *testing.T) {
-	// given
-	model.TruncateTables()
-	corp := createEveEntity(model.EveEntity{Category: model.EveEntityCorporation})
-	c := model.Character{ID: 1, Name: "Erik", Corporation: corp}
-	// when
-	err := c.Save()
-	// then
-	if assert.NoError(t, err) {
-		r, err := model.FetchCharacter(c.ID)
+func TestCharacter(t *testing.T) {
+	t.Run("can save new", func(t *testing.T) {
+		// given
+		model.TruncateTables()
+		corp := createEveEntity(model.EveEntity{Category: model.EveEntityCorporation})
+		c := model.Character{ID: 1, Name: "Erik", Corporation: corp}
+		// when
+		err := c.Save()
+		// then
 		if assert.NoError(t, err) {
-			assert.Equal(t, c, *r)
+			r, err := model.FetchCharacter(c.ID)
+			if assert.NoError(t, err) {
+				assert.Equal(t, c, *r)
+			}
 		}
-	}
-}
+	})
+	t.Run("can update existing", func(t *testing.T) {
+		// given
+		model.TruncateTables()
+		c := createCharacter(model.Character{Name: "Erik"})
+		c.Name = "John"
+		assert.NoError(t, c.Save())
+		// when
+		got, err := model.FetchCharacter(c.ID)
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, c, *got)
+		}
+	})
+	t.Run("can fetch character by ID", func(t *testing.T) {
+		// given
+		model.TruncateTables()
+		createCharacter()
+		c2 := createCharacter()
+		assert.NoError(t, c2.Save())
+		// when
+		r, err := model.FetchCharacter(2)
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, c2, *r)
+		}
+	})
+	t.Run("can fetch all", func(t *testing.T) {
+		// given
+		model.TruncateTables()
+		c1 := createCharacter()
+		c2 := createCharacter()
+		assert.NoError(t, c2.Save())
+		// when
+		got, err := model.FetchAllCharacters()
+		// then
+		if assert.NoError(t, err) {
+			assert.Len(t, got, 2)
+			gotIDs := set.NewFromSlice([]int32{got[0].ID, got[1].ID})
+			wantIDs := set.NewFromSlice([]int32{c1.ID, c2.ID})
+			assert.Equal(t, wantIDs, gotIDs)
 
-func TestCharacterCanUpdate(t *testing.T) {
-	// given
-	model.TruncateTables()
-	c := createCharacter(model.Character{Name: "Erik"})
-	c.Name = "John"
-	assert.NoError(t, c.Save())
-	// when
-	got, err := model.FetchCharacter(c.ID)
-	// then
-	if assert.NoError(t, err) {
-		assert.Equal(t, c, *got)
-	}
-}
-
-func TestCharacterCanFetchByCharacterID(t *testing.T) {
-	// given
-	model.TruncateTables()
-	createCharacter()
-	c2 := createCharacter()
-	assert.NoError(t, c2.Save())
-	// when
-	r, err := model.FetchCharacter(2)
-	// then
-	if assert.NoError(t, err) {
-		assert.Equal(t, c2, *r)
-	}
-}
-
-func TestCharacterCanFetchAll(t *testing.T) {
-	// given
-	model.TruncateTables()
-	c1 := createCharacter()
-	c2 := createCharacter()
-	assert.NoError(t, c2.Save())
-	// when
-	got, err := model.FetchAllCharacters()
-	// then
-	if assert.NoError(t, err) {
-		assert.Len(t, got, 2)
-		gotIDs := set.NewFromSlice([]int32{got[0].ID, got[1].ID})
-		wantIDs := set.NewFromSlice([]int32{c1.ID, c2.ID})
-		assert.Equal(t, wantIDs, gotIDs)
-
-	}
-}
-
-func TestCharacterCanDelete(t *testing.T) {
-	// given
-	model.TruncateTables()
-	c := createCharacter()
-	// when
-	err := c.Delete()
-	// then
-	if assert.NoError(t, err) {
-		_, err := model.FetchCharacter(c.ID)
-		assert.Error(t, err)
-	}
+		}
+	})
+	t.Run("can delete", func(t *testing.T) {
+		// given
+		model.TruncateTables()
+		c := createCharacter()
+		// when
+		err := c.Delete()
+		// then
+		if assert.NoError(t, err) {
+			_, err := model.FetchCharacter(c.ID)
+			assert.Error(t, err)
+		}
+	})
 }
