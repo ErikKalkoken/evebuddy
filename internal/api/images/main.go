a5892efb3e6b9db0b82a41a694aee557afadd57e
ErikKalkoken <erik.kalkoken@gmail.com>
2024-03-27 02:58:26 +0100

    Add images.CorporationLogoURL

---
+++ internal/api/images/main.go
@@ -0,0 +1,50 @@
+// Package images provides image URLs for the Eve Online image server.
+package images
+
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/storage"
+)
+
+const (
+	categoryCharacter   = "characters"
+	categoryCorporation = "corporations"
+)
+
+const PlaceholderCharacterID = 1
+const baseURL = "https://images.evetech.net"
+
+// CharacterPortraitURL returns an image URL for a character portrait
+func CharacterPortraitURL(id int32, size int) (fyne.URI, error) {
+	return imageURL(categoryCharacter, id, size)
+}
+
+// CorporationLogoURL returns an image URL for a corporation logo
+func CorporationLogoURL(id int32, size int) (fyne.URI, error) {
+	return imageURL(categoryCorporation, id, size)
+}
+
+func imageURL(category string, id int32, size int) (fyne.URI, error) {
+	switch size {
+	case 32, 64, 128, 256, 512, 1024:
+		// valid size
+	default:
+		return nil, fmt.Errorf("invalid size %d", size)
+	}
+	category2Class := map[string]string{
+		categoryCharacter:   "portrait",
+		categoryCorporation: "logo",
+	}
+	class, ok := category2Class[category]
+	if !ok {
+		return nil, fmt.Errorf("class not defined for this category: %v", category)
+	}
+	s := fmt.Sprintf("%s/%s/%d/%s?size=%d", baseURL, category, id, class, size)
+	u, err := storage.ParseURI(s)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
---
+++ internal/api/images/main_test.go
@@ -0,0 +1,58 @@
+package images_test
+
+import (
+	"example/esiapp/internal/api/images"
+	"fmt"
+
+	"testing"
+
+	"fyne.io/fyne/v2/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+var testCases = []struct {
+	id    int32
+	size  int
+	valid bool
+}{
+	{42, -1, false},
+	{42, 0, false},
+	{42, 16, false},
+	{42, 32, true},
+	{42, 64, true},
+	{42, 128, true},
+	{42, 256, true},
+	{42, 512, true},
+	{42, 1024, true},
+	{42, 2048, false},
+}
+
+func TestCharacterPortraitURL(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("characterID:%d sizeID:%d", tc.id, tc.size), func(t *testing.T) {
+			got, err := images.CharacterPortraitURL(tc.id, tc.size)
+			if tc.valid && assert.NoError(t, err) {
+				s := fmt.Sprintf("https://images.evetech.net/characters/%d/portrait?size=%d", tc.id, tc.size)
+				want, _ := storage.ParseURI(s)
+				assert.Equal(t, want, got)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
+
+func TestCorporationLogoURL(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("characterID:%d sizeID:%d", tc.id, tc.size), func(t *testing.T) {
+			got, err := images.CorporationLogoURL(tc.id, tc.size)
+			if tc.valid && assert.NoError(t, err) {
+				s := fmt.Sprintf("https://images.evetech.net/corporations/%d/logo?size=%d", tc.id, tc.size)
+				want, _ := storage.ParseURI(s)
+				assert.Equal(t, want, got)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
