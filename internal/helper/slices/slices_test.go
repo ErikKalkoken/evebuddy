e50e0d7de6d34e0669ed771b67bd6023c15af4bc
ErikKalkoken <erik.kalkoken@gmail.com>
2024-04-29 12:23:53 +0200

    Add slices tests

---
+++ internal/helper/slices/slices_test.go
@@ -0,0 +1,31 @@
+package slices_test
+
+import (
+	"example/evebuddy/internal/helper/slices"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertNumeric(t *testing.T) {
+	t.Run("can convert int32 to int", func(t *testing.T) {
+		want := []int{1, 2, 3}
+		got := slices.ConvertNumeric[int32, int]([]int32{1, 2, 3})
+		assert.Equal(t, want, got)
+	})
+	t.Run("can convert int32 to int64", func(t *testing.T) {
+		want := []int64{1, 2, 3}
+		got := slices.ConvertNumeric[int32, int64]([]int32{1, 2, 3})
+		assert.Equal(t, want, got)
+	})
+	t.Run("can convert int to int32", func(t *testing.T) {
+		want := []int32{1, 2, 3}
+		got := slices.ConvertNumeric[int, int32]([]int{1, 2, 3})
+		assert.Equal(t, want, got)
+	})
+	t.Run("can convert empty slice", func(t *testing.T) {
+		want := []int32{}
+		got := slices.ConvertNumeric[int, int32]([]int{})
+		assert.Equal(t, want, got)
+	})
+}
