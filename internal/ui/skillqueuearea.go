f48b2c71ec5f238e591d238921e4a8db367e1aa0
ErikKalkoken <erik.kalkoken@gmail.com>
2024-05-07 14:50:56 +0200

    Use List widget for skillqueue

--- internal/ui/skillqueuearea.go
+++ internal/ui/skillqueuearea.go
@@ -11,25 +11,24 @@ import (
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/ErikKalkoken/evebuddy/internal/model"
 	"github.com/ErikKalkoken/evebuddy/internal/service"
 	"github.com/dustin/go-humanize"
 )
 
 // skillqueueArea is the UI area that shows the skillqueue
 type skillqueueArea struct {
-	content fyne.CanvasObject
-	items   *fyne.Container
+	content *fyne.Container
 	ui      *ui
 }
 
 func (u *ui) NewSkillqueueArea() *skillqueueArea {
-	items := container.NewVBox()
-	c := skillqueueArea{ui: u, content: container.NewScroll(items), items: items}
+	c := skillqueueArea{ui: u, content: container.NewStack()}
 	return &c
 }
 
 func (a *skillqueueArea) Redraw() {
-	a.items.RemoveAll()
+	a.content.RemoveAll()
 	characterID := a.ui.CurrentCharID()
 	if characterID == 0 {
 		return
@@ -37,51 +36,64 @@ func (a *skillqueueArea) Redraw() {
 	qq, err := a.ui.service.ListSkillqueue(characterID)
 	if err != nil {
 		t := canvas.NewText("Failed to fetch skillqueue", theme.ErrorColor())
-		a.items.Add(t)
+		a.content.Add(t)
 		return
 	}
 	if len(qq) == 0 {
-		a.items.Add(widget.NewLabel("No data"))
+		a.content.Add(widget.NewLabel("No data"))
 		return
 	}
+
 	now := time.Now()
-	isActive := false
+	qq2 := make([]*model.SkillqueueItem, 0)
 	for _, q := range qq {
-		if q.StartDate.Before(now) && q.FinishDate.After(now) {
-			isActive = true
-			break
-		}
-	}
-	if !isActive {
-		a.items.Add(widget.NewLabel("Skill queue is not active!"))
-		a.items.Add(widget.NewLabel(""))
-	}
-	header := container.NewHBox(widget.NewLabel("Skill"), layout.NewSpacer(), widget.NewLabel("Finished At"))
-	a.items.Add(container.NewStack(
-		canvas.NewRectangle(theme.DisabledButtonColor()),
-		header,
-	))
-	for _, q := range qq {
-		if isActive && q.FinishDate.Before(now) {
+		if q.FinishDate.Before(now) {
 			continue
 		}
-		name := widget.NewLabel(fmt.Sprintf("%s %s", q.SkillName, romanLetter(q.FinishedLevel)))
-		var x string
-		if !q.FinishDate.IsZero() {
-			x = fmt.Sprintf("%s (%s)", q.FinishDate.Format(myDateTime), humanize.Time(q.FinishDate))
-		} else {
-			x = "?"
-		}
-		finished := widget.NewLabel(x)
-		row := container.NewHBox(name, layout.NewSpacer(), finished)
-		wrapper := container.NewStack()
-		if q.StartDate.Before(now) && q.FinishDate.After(now) {
-			wrapper.Add(widget.NewProgressBarInfinite())
-		}
-		wrapper.Add(row)
-		a.items.Add(wrapper)
-		a.items.Add(widget.NewSeparator())
+		qq2 = append(qq2, q)
 	}
+
+	if len(qq2) == 0 {
+		a.content.Add(widget.NewLabel("Skill queue is not active!"))
+		return
+	}
+
+	list := widget.NewList(
+		func() int {
+			return len(qq2)
+		},
+		func() fyne.CanvasObject {
+			x := widget.NewProgressBarInfinite()
+			x.Stop()
+			x.Hide()
+			return container.NewStack(
+				x,
+				container.NewHBox(
+					widget.NewLabel("skill"),
+					layout.NewSpacer(),
+					widget.NewLabel("finished"),
+				))
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			q := qq2[i]
+			row := o.(*fyne.Container).Objects[1].(*fyne.Container)
+			name := fmt.Sprintf("%s %s", q.SkillName, romanLetter(q.FinishedLevel))
+			row.Objects[0].(*widget.Label).SetText(name)
+			var finished string
+			if !q.FinishDate.IsZero() {
+				finished = fmt.Sprintf("%s (%s)", q.FinishDate.Format(myDateTime), humanize.Time(q.FinishDate))
+			} else {
+				finished = "?"
+			}
+			row.Objects[2].(*widget.Label).SetText(finished)
+			progressBar := o.(*fyne.Container).Objects[0].(*widget.ProgressBarInfinite)
+			if q.StartDate.Before(now) && q.FinishDate.After(now) {
+				progressBar.Show()
+				progressBar.Start()
+			}
+		})
+
+	a.content.Add(list)
 }
 
 func romanLetter(v int) string {
