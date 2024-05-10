11348df5390c6aea1aba94da24e971f885994e74
ErikKalkoken <erik.kalkoken@gmail.com>
2024-05-10 14:07:58 +0200

    Use toolbar instead of button for mails

--- internal/ui/characterarea.go
+++ internal/ui/characterarea.go
@@ -32,8 +32,6 @@ type characterArea struct {
 	ui      *ui
 }
 
-// TODO: Replace redraw approach with refresh
-
 func (u *ui) NewCharacterArea() *characterArea {
 	items := container.NewVBox()
 	c := characterArea{ui: u, content: container.NewScroll(items), items: items}
--- internal/ui/maildetailarea.go
+++ internal/ui/maildetailarea.go
@@ -7,7 +7,6 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/ErikKalkoken/evebuddy/internal/model"
@@ -15,48 +14,45 @@ import (
 
 // mailDetailArea is the UI area showing the current mail.
 type mailDetailArea struct {
-	actions *fyne.Container
 	body    *widget.RichText
 	content fyne.CanvasObject
 	header  *widget.Label
 	mail    *model.Mail
 	subject *widget.Label
+	toolbar *widget.Toolbar
 	ui      *ui
 }
 
 func (u *ui) NewMailArea() *mailDetailArea {
 	a := mailDetailArea{ui: u}
-	actions := container.NewHBox(
-		widget.NewButtonWithIcon("", theme.MailReplyIcon(), func() {
+	a.toolbar = widget.NewToolbar(
+		widget.NewToolbarAction(theme.MailReplyIcon(), func() {
 			u.ShowSendMessageWindow(CreateMessageReply, a.mail)
 		}),
-		widget.NewButtonWithIcon("", theme.MailReplyAllIcon(), func() {
+		widget.NewToolbarAction(theme.MailReplyAllIcon(), func() {
 			u.ShowSendMessageWindow(CreateMessageReplyAll, a.mail)
 		}),
-		widget.NewButtonWithIcon("", theme.MailForwardIcon(), func() {
+		widget.NewToolbarAction(theme.MailForwardIcon(), func() {
 			u.ShowSendMessageWindow(CreateMessageForward, a.mail)
 		}),
-		layout.NewSpacer(),
-	)
-	buttonDelete := widget.NewButtonWithIcon("", theme.DeleteIcon(), func() {
-		t := fmt.Sprintf("Are you sure you want to delete this mail?\n\n%s", a.mail.Subject)
-		d := dialog.NewConfirm("Delete mail", t, func(confirmed bool) {
-			if confirmed {
-				err := u.service.DeleteMail(a.mail.MyCharacterID, a.mail.MailID)
-				if err != nil {
-					errorDialog := dialog.NewError(err, u.window)
-					errorDialog.Show()
-				} else {
-					u.headerArea.Refresh()
+		widget.NewToolbarSpacer(),
+		widget.NewToolbarAction(theme.DeleteIcon(), func() {
+			t := fmt.Sprintf("Are you sure you want to delete this mail?\n\n%s", a.mail.Subject)
+			d := dialog.NewConfirm("Delete mail", t, func(confirmed bool) {
+				if confirmed {
+					err := u.service.DeleteMail(a.mail.MyCharacterID, a.mail.MailID)
+					if err != nil {
+						errorDialog := dialog.NewError(err, u.window)
+						errorDialog.Show()
+					} else {
+						u.headerArea.Refresh()
+					}
 				}
-			}
-		}, u.window)
-		d.Show()
-	})
-	buttonDelete.Importance = widget.DangerImportance
-	actions.Add(buttonDelete)
-	a.actions = actions
-	a.toogleActions(false)
+			}, u.window)
+			d.Show()
+		}),
+	)
+	a.toolbar.Hide()
 
 	subject := widget.NewLabel("")
 	subject.TextStyle = fyne.TextStyle{Bold: true}
@@ -67,7 +63,7 @@ func (u *ui) NewMailArea() *mailDetailArea {
 	header.Truncation = fyne.TextTruncateEllipsis
 	a.header = header
 
-	wrapper := container.NewVBox(actions, subject, header)
+	wrapper := container.NewVBox(a.toolbar, subject, header)
 
 	body := widget.NewRichText()
 	body.Wrapping = fyne.TextWrapBreak
@@ -79,7 +75,7 @@ func (u *ui) NewMailArea() *mailDetailArea {
 
 func (a *mailDetailArea) Clear() {
 	a.updateContent("", "", "")
-	a.toogleActions(false)
+	a.toolbar.Hide()
 }
 
 func (a *mailDetailArea) SetMail(mailID int32, listItemID widget.ListItemID) {
@@ -108,18 +104,7 @@ func (a *mailDetailArea) SetMail(mailID int32, listItemID widget.ListItemID) {
 
 	header := a.mail.MakeHeaderText(myDateTime)
 	a.updateContent(a.mail.Subject, header, a.mail.BodyToMarkdown())
-	a.toogleActions(true)
-}
-
-func (a *mailDetailArea) toogleActions(enabled bool) {
-	for _, i := range []int{0, 1, 2, 4} {
-		b := a.actions.Objects[i].(*widget.Button)
-		if enabled {
-			b.Enable()
-		} else {
-			b.Disable()
-		}
-	}
+	a.toolbar.Show()
 }
 
 func (a *mailDetailArea) updateContent(s string, h string, b string) {
