4cf637aeb3a81d28c1cd49513030534951e8bc47
ErikKalkoken <erik.kalkoken@gmail.com>
2024-05-15 12:32:30 +0200

    Add token tests

---
+++ internal/model/token_test.go
@@ -0,0 +1,20 @@
+package model_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/ErikKalkoken/evebuddy/internal/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTokenRemainsValid(t *testing.T) {
+	t.Run("return true, when token remains valid within duration", func(t *testing.T) {
+		x := model.Token{ExpiresAt: time.Now().Add(60 * time.Second)}
+		assert.True(t, x.RemainsValid(55*time.Second))
+	})
+	t.Run("return false, when token expired within duration", func(t *testing.T) {
+		x := model.Token{ExpiresAt: time.Now().Add(60 * time.Second)}
+		assert.False(t, x.RemainsValid(65*time.Second))
+	})
+}
