0d9788d368c084dd338d311c0ff9b817280861d3
ErikKalkoken <erik.kalkoken@gmail.com>
2024-06-01 22:09:35 +0200

    Improve esi paging helper

--- internal/helper/goesi/goesi.go
+++ internal/helper/goesi/goesi.go
@@ -12,32 +12,33 @@ import (
 // FetchFromESIWithPaging returns the combined list of items from all pages of an ESI endpoint.
 // This only works for ESI endpoints which support the X-Pages pattern and return a list.
 func FetchFromESIWithPaging[T any](fetch func(int) ([]T, *http.Response, error)) ([]T, error) {
-	results := make(map[int][]T)
 	result, r, err := fetch(1)
 	if err != nil {
 		return nil, err
 	}
-	results[1] = result
 	pages, err := extractPageCount(r)
 	if err != nil {
 		return nil, err
 	}
-	if pages > 1 {
-		g := new(errgroup.Group)
-		for p := 2; p <= pages; p++ {
-			p := p
-			g.Go(func() error {
-				result, _, err := fetch(p)
-				if err != nil {
-					return err
-				}
-				results[p] = result
-				return nil
-			})
-		}
-		if err := g.Wait(); err != nil {
-			return nil, err
-		}
+	if pages < 2 {
+		return result, nil
+	}
+	results := make([][]T, pages)
+	results[0] = result
+	g := new(errgroup.Group)
+	for p := 2; p <= pages; p++ {
+		p := p
+		g.Go(func() error {
+			result, _, err := fetch(p)
+			if err != nil {
+				return err
+			}
+			results[p-1] = result
+			return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		return nil, err
 	}
 	combined := make([]T, 0)
 	for _, result := range results {
---
+++ internal/helper/goesi/goesi_test.go
@@ -0,0 +1,182 @@
+package goesi_test
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"testing"
+
+	igoesi "github.com/ErikKalkoken/evebuddy/internal/helper/goesi"
+	"github.com/antihax/goesi"
+	"github.com/antihax/goesi/esi"
+	"github.com/antihax/goesi/optional"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFetchFromESIWithPaging(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	client := goesi.NewAPIClient(nil, "")
+	ctx := context.Background()
+	t.Run("should fetch multiple pages", func(t *testing.T) {
+		// given
+		httpmock.Reset()
+		pages := "3"
+		httpmock.RegisterResponder(
+			"GET",
+			"https://esi.evetech.net/v5/characters/99/assets/",
+			httpmock.NewJsonResponderOrPanic(200, []map[string]interface{}{
+				{
+					"is_blueprint_copy": true,
+					"is_singleton":      true,
+					"item_id":           1000000016835,
+					"location_flag":     "Hangar",
+					"location_id":       60002959,
+					"location_type":     "station",
+					"quantity":          1,
+					"type_id":           3516,
+				},
+			}).HeaderSet(http.Header{"X-Pages": []string{pages}}))
+		httpmock.RegisterResponder(
+			"GET",
+			"https://esi.evetech.net/v5/characters/99/assets/?page=2",
+			httpmock.NewJsonResponderOrPanic(200, []map[string]interface{}{
+				{
+					"is_blueprint_copy": true,
+					"is_singleton":      false,
+					"item_id":           1000000016836,
+					"location_flag":     "Hangar",
+					"location_id":       60002959,
+					"location_type":     "station",
+					"quantity":          1,
+					"type_id":           3516,
+				},
+			}).HeaderSet(http.Header{"X-Pages": []string{pages}}))
+		httpmock.RegisterResponder(
+			"GET",
+			"https://esi.evetech.net/v5/characters/99/assets/?page=3",
+			httpmock.NewJsonResponderOrPanic(200, []map[string]interface{}{
+				{
+					"is_blueprint_copy": true,
+					"is_singleton":      false,
+					"item_id":           1000000016837,
+					"location_flag":     "Hangar",
+					"location_id":       60002959,
+					"location_type":     "station",
+					"quantity":          1,
+					"type_id":           3516,
+				},
+			}).HeaderSet(http.Header{"X-Pages": []string{pages}}))
+		// when
+		xx, err := igoesi.FetchFromESIWithPaging(
+			func(pageNum int) ([]esi.GetCharactersCharacterIdAssets200Ok, *http.Response, error) {
+				arg := &esi.GetCharactersCharacterIdAssetsOpts{
+					Page: optional.NewInt32(int32(pageNum)),
+				}
+				return client.ESI.AssetsApi.GetCharactersCharacterIdAssets(ctx, 99, arg)
+			})
+		// then
+		if assert.NoError(t, err) {
+			assert.Len(t, xx, 3)
+			want := []int64{1000000016835, 1000000016836, 1000000016837}
+			got := make([]int64, 3)
+			for i, x := range xx {
+				got[i] = x.ItemId
+			}
+			assert.Equal(t, want, got)
+		}
+	})
+	t.Run("should fetch single page", func(t *testing.T) {
+		// given
+		httpmock.Reset()
+		pages := "1"
+		httpmock.RegisterResponder(
+			"GET",
+			"https://esi.evetech.net/v5/characters/99/assets/",
+			httpmock.NewJsonResponderOrPanic(200, []map[string]interface{}{
+				{
+					"is_blueprint_copy": true,
+					"is_singleton":      true,
+					"item_id":           1000000016835,
+					"location_flag":     "Hangar",
+					"location_id":       60002959,
+					"location_type":     "station",
+					"quantity":          1,
+					"type_id":           3516,
+				},
+			}).HeaderSet(http.Header{"X-Pages": []string{pages}}))
+		// when
+		xx, err := igoesi.FetchFromESIWithPaging(
+			func(pageNum int) ([]esi.GetCharactersCharacterIdAssets200Ok, *http.Response, error) {
+				arg := &esi.GetCharactersCharacterIdAssetsOpts{
+					Page: optional.NewInt32(int32(pageNum)),
+				}
+				return client.ESI.AssetsApi.GetCharactersCharacterIdAssets(ctx, 99, arg)
+			})
+		// then
+		if assert.NoError(t, err) {
+			assert.Len(t, xx, 1)
+		}
+	})
+	t.Run("can ignore missing x-pages header", func(t *testing.T) {
+		// given
+		httpmock.Reset()
+		httpmock.RegisterResponder(
+			"GET",
+			"https://esi.evetech.net/v5/characters/99/assets/",
+			httpmock.NewJsonResponderOrPanic(200, []map[string]interface{}{
+				{
+					"is_blueprint_copy": true,
+					"is_singleton":      true,
+					"item_id":           1000000016835,
+					"location_flag":     "Hangar",
+					"location_id":       60002959,
+					"location_type":     "station",
+					"quantity":          1,
+					"type_id":           3516,
+				},
+			}))
+		// when
+		xx, err := igoesi.FetchFromESIWithPaging(
+			func(pageNum int) ([]esi.GetCharactersCharacterIdAssets200Ok, *http.Response, error) {
+				arg := &esi.GetCharactersCharacterIdAssetsOpts{
+					Page: optional.NewInt32(int32(pageNum)),
+				}
+				return client.ESI.AssetsApi.GetCharactersCharacterIdAssets(ctx, 99, arg)
+			})
+		// then
+		if assert.NoError(t, err) {
+			assert.Len(t, xx, 1)
+		}
+	})
+	t.Run("should return error from function", func(t *testing.T) {
+		// given
+		myErr := errors.New("error")
+		// when
+		_, err := igoesi.FetchFromESIWithPaging(
+			func(pageNum int) ([]int, *http.Response, error) {
+				return nil, nil, myErr
+			})
+		// then
+		assert.ErrorIs(t, err, myErr)
+	})
+	t.Run("should return error when X-Pages is invalid", func(t *testing.T) {
+		// given
+		httpmock.Reset()
+		httpmock.RegisterResponder(
+			"GET",
+			"https://esi.evetech.net/v5/characters/99/assets/",
+			httpmock.NewJsonResponderOrPanic(200, []map[string]interface{}{}).HeaderSet(http.Header{"X-Pages": []string{"invalid"}}))
+		// when
+		_, err := igoesi.FetchFromESIWithPaging(
+			func(pageNum int) ([]esi.GetCharactersCharacterIdAssets200Ok, *http.Response, error) {
+				arg := &esi.GetCharactersCharacterIdAssetsOpts{
+					Page: optional.NewInt32(int32(pageNum)),
+				}
+				return client.ESI.AssetsApi.GetCharactersCharacterIdAssets(ctx, 99, arg)
+			})
+		// then
+		assert.Error(t, err)
+	})
+}
