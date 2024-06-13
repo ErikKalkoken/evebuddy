0361be4ff514cf2b854607953f284c7242eb71de
ErikKalkoken <erik.kalkoken@gmail.com>
2024-06-13 13:21:21 +0200

    Refactor assets ui

--- internal/ui/assets.go
+++ internal/ui/assets.go
@@ -35,12 +35,12 @@ const (
 // locationDataTree represents a tree of location nodes for rendering with the tree widget.
 type locationDataTree struct {
 	ids    map[string][]string
-	values map[string]string
+	values map[string]locationDataNode
 }
 
 func newLocationDataTree() locationDataTree {
 	ltd := locationDataTree{
-		values: make(map[string]string),
+		values: make(map[string]locationDataNode),
 		ids:    make(map[string][]string),
 	}
 	return ltd
@@ -59,12 +59,20 @@ func (ltd locationDataTree) add(parentUID string, node locationDataNode) string 
 		panic(fmt.Sprintf("UID for this node already exists: %v", node))
 	}
 	ltd.ids[parentUID] = append(ltd.ids[parentUID], uid)
-	ltd.values[uid] = objectToJSONOrPanic(node)
+	ltd.values[uid] = node
 	return uid
 }
 
-func (ltd locationDataTree) stringTree() (map[string][]string, map[string]string) {
-	return ltd.ids, ltd.values
+func (ltd locationDataTree) stringTree() (map[string][]string, map[string]string, error) {
+	values := make(map[string]string)
+	for id, node := range ltd.values {
+		v, err := objectToJSON(node)
+		if err != nil {
+			return nil, nil, err
+		}
+		values[id] = v
+	}
+	return ltd.ids, values, nil
 }
 
 // locationDataNode is a node for the asset tree widget.
@@ -194,7 +202,10 @@ func (a *assetsArea) redraw() {
 		if err != nil {
 			return "", 0, err
 		}
-		ids, values := tree.stringTree()
+		ids, values, err := tree.stringTree()
+		if err != nil {
+			return "", 0, err
+		}
 		if err := a.locationsData.Set(ids, values); err != nil {
 			return "", 0, err
 		}
