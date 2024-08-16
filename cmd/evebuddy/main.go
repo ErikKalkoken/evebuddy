d2fbd5d61e53101780e504b6246a04e1250e633e
ErikKalkoken <erik.kalkoken@gmail.com>
2024-08-16 21:49:41 +0200

    Add missing cmd folder to repo

---
+++ cmd/evebuddy/main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/natefinch/lumberjack.v2"
+
+	"github.com/ErikKalkoken/evebuddy/internal/app/character"
+	"github.com/ErikKalkoken/evebuddy/internal/app/esistatus"
+	"github.com/ErikKalkoken/evebuddy/internal/app/evenotification"
+	"github.com/ErikKalkoken/evebuddy/internal/app/eveuniverse"
+	"github.com/ErikKalkoken/evebuddy/internal/app/statuscache"
+	"github.com/ErikKalkoken/evebuddy/internal/app/storage"
+	"github.com/ErikKalkoken/evebuddy/internal/app/ui"
+	"github.com/ErikKalkoken/evebuddy/internal/cache"
+	"github.com/ErikKalkoken/evebuddy/internal/eveimage"
+	"github.com/ErikKalkoken/evebuddy/internal/httptransport"
+	"github.com/ErikKalkoken/evebuddy/internal/sso"
+	"github.com/antihax/goesi"
+	"github.com/chasinglogic/appdirs"
+)
+
+const (
+	ssoClientId = "11ae857fe4d149b2be60d875649c05f1"
+)
+
+type logLevelFlag struct {
+	value slog.Level
+}
+
+func (l logLevelFlag) String() string {
+	return l.value.String()
+}
+
+func (l *logLevelFlag) Set(value string) error {
+	m := map[string]slog.Level{"DEBUG": slog.LevelDebug, "INFO": slog.LevelInfo, "WARN": slog.LevelWarn, "ERROR": slog.LevelError}
+	v, ok := m[strings.ToUpper(value)]
+	if !ok {
+		return fmt.Errorf("unknown log level")
+	}
+	l.value = v
+	return nil
+}
+
+// defined flags
+var (
+	levelFlag    logLevelFlag
+	debugFlag    = flag.Bool("debug", false, "Show additional debug information")
+	logFileFlag  = flag.Bool("logfile", false, "Write logs to a file instead of the console")
+	removeFlag   = flag.Bool("remove-user-files", false, "Remove all user files of this app")
+	showDirsFlag = flag.Bool("show-dirs", false, "Show directories where data is stored")
+)
+
+func init() {
+	levelFlag.value = slog.LevelWarn
+	flag.Var(&levelFlag, "loglevel", "set log level")
+}
+
+func main() {
+	flag.Parse()
+	slog.SetLogLoggerLevel(levelFlag.value)
+	log.SetFlags(log.LstdFlags | log.Llongfile)
+	ad := appdirs.New("evebuddy")
+	if *showDirsFlag {
+		fmt.Printf("Database: %s\n", ad.UserData())
+		fmt.Printf("Cache: %s\n", ad.UserCache())
+		fmt.Printf("Logs: %s\n", ad.UserLog())
+		return
+	}
+	if *removeFlag {
+		fmt.Print("Are you sure you want to remove all locally stored data of this app (y/N)?")
+		var input string
+		fmt.Scanln(&input)
+		if strings.ToLower(input) == "y" {
+			pp := []struct {
+				name string
+				path string
+			}{
+				{"user data", ad.UserData()},
+				{"user log", ad.UserLog()},
+				{"user cache", ad.UserCache()},
+			}
+			for _, p := range pp {
+				if err := os.RemoveAll(p.path); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("Deleted %s: %s\n", p.name, p.path)
+			}
+		} else {
+			fmt.Println("Aborted")
+		}
+		return
+	}
+	if *logFileFlag {
+		fn, err := makeLogFileName(ad)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.SetOutput(&lumberjack.Logger{
+			Filename:   fn,
+			MaxSize:    50, // megabytes
+			MaxBackups: 3,
+		})
+	}
+	dsn, err := makeDSN(ad)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := storage.InitDB(dsn)
+	if err != nil {
+		log.Fatalf("Failed to initialize database %s: %s", dsn, err)
+	}
+	defer db.Close()
+	st := storage.New(db)
+	httpClient := &http.Client{
+		Transport: httptransport.LoggedTransport{},
+	}
+	esiHttpClient := &http.Client{
+		Transport: httptransport.LoggedTransportWithRetries{
+			MaxRetries: 3,
+			StatusCodesToRetry: []int{
+				http.StatusBadGateway,
+				http.StatusGatewayTimeout,
+				http.StatusServiceUnavailable,
+			},
+		},
+	}
+	userAgent := "EveBuddy kalkoken87@gmail.com"
+	esiClient := goesi.NewAPIClient(esiHttpClient, userAgent)
+	cache := cache.New()
+	sc := statuscache.New(cache)
+	if err := sc.InitCache(context.TODO(), st); err != nil {
+		panic(err)
+	}
+
+	eu := eveuniverse.New(st, esiClient)
+	eu.StatusCacheService = sc
+
+	en := evenotification.New()
+	en.EveUniverseService = eu
+
+	cs := character.New(st, httpClient, esiClient)
+	cs.EveNotificationService = en
+	cs.EveUniverseService = eu
+	cs.StatusCacheService = sc
+	cs.SSOService = sso.New(ssoClientId, httpClient, cache)
+
+	imageCacheDir, err := makeImageCachePath(ad)
+	if err != nil {
+		log.Fatal(err)
+	}
+	u := ui.NewUI(*debugFlag)
+	u.CacheService = cache
+	u.CharacterService = cs
+	u.ESIStatusService = esistatus.New(esiClient)
+	u.EveImageService = eveimage.New(imageCacheDir, httpClient)
+	u.EveUniverseService = eu
+	u.StatusCacheService = sc
+	u.Init()
+	u.ShowAndRun()
+}
+
+func makeLogFileName(ad *appdirs.App) (string, error) {
+	fn := "evebuddy.log"
+	if err := os.MkdirAll(ad.UserLog(), os.ModePerm); err != nil {
+		return "", err
+	}
+	path := fmt.Sprintf("%s/%s", ad.UserLog(), fn)
+	return path, nil
+}
+
+func makeDSN(ad *appdirs.App) (string, error) {
+	fn := "evebuddy.sqlite"
+	path := ad.UserData()
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		return "", err
+	}
+	dsn := fmt.Sprintf("file:%s/%s", path, fn)
+	return dsn, nil
+}
+
+func makeImageCachePath(ad *appdirs.App) (string, error) {
+	p := filepath.Join(ad.UserCache(), "images")
+	if err := os.MkdirAll(p, os.ModePerm); err != nil {
+		return "", err
+	}
+	return p, nil
+}
