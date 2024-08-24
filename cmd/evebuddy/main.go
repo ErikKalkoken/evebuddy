c4fa794217711de0d3f6061079526a0cc557becd
ErikKalkoken <erik.kalkoken@gmail.com>
2024-08-24 20:09:07 +0200

    Refactor main package

--- cmd/evebuddy/main.go
+++ cmd/evebuddy/main.go
@@ -32,24 +32,6 @@ const (
 	ssoClientId = "11ae857fe4d149b2be60d875649c05f1"
 )
 
-type logLevelFlag struct {
-	value slog.Level
-}
-
-func (l logLevelFlag) String() string {
-	return l.value.String()
-}
-
-func (l *logLevelFlag) Set(value string) error {
-	m := map[string]slog.Level{"DEBUG": slog.LevelDebug, "INFO": slog.LevelInfo, "WARN": slog.LevelWarn, "ERROR": slog.LevelError}
-	v, ok := m[strings.ToUpper(value)]
-	if !ok {
-		return fmt.Errorf("unknown log level")
-	}
-	l.value = v
-	return nil
-}
-
 // defined flags
 var (
 	levelFlag    logLevelFlag
