e9ef77783827c7d9adb418cd379e54be6dbed767
ErikKalkoken <erik.kalkoken@gmail.com>
2025-04-22 19:11:41 +0200

    Fix test bug

--- internal/app/assetcollection/assetcollection_internal_test.go
+++ internal/app/assetcollection/assetcollection_internal_test.go
@@ -21,7 +21,7 @@ func TestBaseNode(t *testing.T) {
 	n1.Add(d)
 	t.Run("can return nodes", func(t *testing.T) {
 		got := ln.Nodes()
-		assert.EqualValues(t, []int{1, 2}, xslices.Map(got, func(a AssetNode) int {
+		assert.ElementsMatch(t, []int{1, 2}, xslices.Map(got, func(a AssetNode) int {
 			return int(a.Asset.ItemID)
 		}))
 	})
