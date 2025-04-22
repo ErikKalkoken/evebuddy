5c805fa937228b1b2da7f6ab574086ed1436594c
ErikKalkoken <erik.kalkoken@gmail.com>
2025-04-23 00:28:59 +0200

    Refactor evenotification package

--- internal/app/evenotification/evenotification_internal_test.go
+++ internal/app/evenotification/evenotification_internal_test.go
@@ -3,6 +3,7 @@ package evenotification
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/ErikKalkoken/evebuddy/internal/app/eveuniverseservice"
 	"github.com/ErikKalkoken/evebuddy/internal/app/storage/testutil"
@@ -51,5 +52,16 @@ func TestMakeStructureBaseText(t *testing.T) {
 			assert.Empty(t, x.owner)
 			assert.NotEmpty(t, x.intro)
 		}
+	})
+}
+
+func TestLDAPTimeConversion(t *testing.T) {
+	t.Run("should convert LDAP time", func(t *testing.T) {
+		x := fromLDAPTime(131924601300000000)
+		assert.Equal(t, time.Date(2019, 1, 20, 12, 15, 30, 0, time.UTC), x)
+	})
+	t.Run("should convert LDAP duration", func(t *testing.T) {
+		x := fromLDAPDuration(9000000000)
+		assert.Equal(t, time.Duration(15*time.Minute), x)
 	})
 }
