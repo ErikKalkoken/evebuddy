8fcc5a446d2814c25b6ef3493db1a94a8e4dd410
ErikKalkoken <erik.kalkoken@gmail.com>
2025-05-31 16:05:10 +0200

    refactor (eveuniverse): move location logic in own file

--- internal/app/eveuniverseservice/type_test.go
+++ internal/app/eveuniverseservice/type_test.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"testing"
 
+	"github.com/ErikKalkoken/evebuddy/internal/app"
 	"github.com/ErikKalkoken/evebuddy/internal/app/eveuniverseservice"
 	"github.com/ErikKalkoken/evebuddy/internal/app/storage"
 	"github.com/ErikKalkoken/evebuddy/internal/app/storage/testutil"
+	"github.com/ErikKalkoken/evebuddy/internal/set"
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
 )
@@ -107,5 +109,228 @@ func TestGetOrCreateEveGroupESI(t *testing.T) {
 				assert.Equal(t, x1, x2)
 			}
 		}
+	})
+}
+
+func TestGetOrCreateEveTypeESI(t *testing.T) {
+	db, st, factory := testutil.New()
+	defer db.Close()
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	s := eveuniverseservice.NewTestService(st)
+	ctx := context.Background()
+	t.Run("should return existing type", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		httpmock.Reset()
+		factory.CreateEveType(storage.CreateEveTypeParams{ID: 587})
+		// when
+		x1, err := s.GetOrCreateTypeESI(ctx, 587)
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, int32(587), x1.ID)
+		}
+	})
+	t.Run("should fetch type from ESI and create it", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		httpmock.Reset()
+		factory.CreateEveGroup(storage.CreateEveGroupParams{ID: 25})
+		factory.CreateEveDogmaAttribute(storage.CreateEveDogmaAttributeParams{ID: 161})
+		factory.CreateEveDogmaAttribute(storage.CreateEveDogmaAttributeParams{ID: 162})
+		httpmock.RegisterResponder(
+			"GET",
+			`=~^https://esi\.evetech\.net/v\d+/universe/types/\d+/`,
+			httpmock.NewJsonResponderOrPanic(200, map[string]any{
+				"description": "The Rifter is a...",
+				"dogma_attributes": []map[string]any{
+					{
+						"attribute_id": 161,
+						"value":        11,
+					},
+					{
+						"attribute_id": 162,
+						"value":        12,
+					},
+				},
+				"dogma_effects": []map[string]any{
+					{
+						"effect_id":  111,
+						"is_default": true,
+					},
+					{
+						"effect_id":  112,
+						"is_default": false,
+					},
+				},
+				"group_id":  25,
+				"name":      "Rifter",
+				"published": true,
+				"type_id":   587,
+			}),
+		)
+		// when
+		x1, err := s.GetOrCreateTypeESI(ctx, 587)
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, int32(587), x1.ID)
+			assert.Equal(t, "Rifter", x1.Name)
+			assert.Equal(t, int32(25), x1.Group.ID)
+			assert.Equal(t, true, x1.IsPublished)
+			x2, err := st.GetEveType(ctx, 587)
+			if assert.NoError(t, err) {
+				assert.Equal(t, x1, x2)
+			}
+			y, err := st.GetEveTypeDogmaAttribute(ctx, 587, 161)
+			if assert.NoError(t, err) {
+				assert.Equal(t, float32(11), y)
+			}
+			z, err := st.GetEveTypeDogmaEffect(ctx, 587, 111)
+			if assert.NoError(t, err) {
+				assert.True(t, z)
+			}
+
+		}
+	})
+	t.Run("should fetch group from ESI and create it (integration)", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		httpmock.Reset()
+		httpmock.RegisterResponder(
+			"GET",
+			`=~^https://esi\.evetech\.net/v\d+/universe/categories/\d+/`,
+			httpmock.NewJsonResponderOrPanic(200, map[string]any{
+				"category_id": 6,
+				"groups":      []int{25, 26, 27},
+				"name":        "Ship",
+				"published":   true,
+			}),
+		)
+		httpmock.RegisterResponder(
+			"GET",
+			`=~^https://esi\.evetech\.net/v\d+/universe/groups/\d+/`,
+			httpmock.NewJsonResponderOrPanic(200, map[string]any{
+				"category_id": 6,
+				"group_id":    25,
+				"name":        "Frigate",
+				"published":   true,
+				"types":       []int{587, 586, 585},
+			}),
+		)
+		httpmock.RegisterResponder(
+			"GET",
+			`=~^https://esi\.evetech\.net/v\d+/universe/types/\d+/`,
+			httpmock.NewJsonResponderOrPanic(200, map[string]any{
+				"description": "The Rifter is a...",
+				"group_id":    25,
+				"name":        "Rifter",
+				"published":   true,
+				"type_id":     587,
+			}),
+		)
+		// when
+		x1, err := s.GetOrCreateTypeESI(ctx, 587)
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, int32(587), x1.ID)
+			assert.Equal(t, "Rifter", x1.Name)
+			assert.Equal(t, int32(25), x1.Group.ID)
+			assert.Equal(t, true, x1.IsPublished)
+			x2, err := st.GetEveType(ctx, 587)
+			if assert.NoError(t, err) {
+				assert.Equal(t, x1, x2)
+			}
+		}
+	})
+}
+
+func TestAddMissingEveTypes(t *testing.T) {
+	db, st, factory := testutil.New()
+	defer db.Close()
+	ctx := context.Background()
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	s := eveuniverseservice.NewTestService(st)
+	t.Run("do nothing when all types already exist", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		httpmock.Reset()
+		x1 := factory.CreateEveType()
+		// when
+		err := s.AddMissingTypes(ctx, set.Of(x1.ID))
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, 0, httpmock.GetTotalCallCount())
+		}
+	})
+}
+
+func TestGetOrCreateEveRaceESI(t *testing.T) {
+	db, st, factory := testutil.New()
+	defer db.Close()
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	s := eveuniverseservice.NewTestService(st)
+	ctx := context.Background()
+	t.Run("should return existing race", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		httpmock.Reset()
+		x1 := factory.CreateEveRace(app.EveRace{ID: 7})
+		// when
+		x2, err := s.GetOrCreateRaceESI(ctx, 7)
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, x1, x2)
+		}
+	})
+	t.Run("should create race from ESI when it does not exit in DB", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		httpmock.Reset()
+		httpmock.RegisterResponder(
+			"GET",
+			"https://esi.evetech.net/v1/universe/races/",
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{
+				{
+					"alliance_id": 500001,
+					"description": "Founded on the tenets of patriotism and hard work...",
+					"name":        "Caldari",
+					"race_id":     7,
+				},
+			}))
+
+		// when
+		x1, err := s.GetOrCreateRaceESI(ctx, 7)
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, "Caldari", x1.Name)
+			assert.Equal(t, "Founded on the tenets of patriotism and hard work...", x1.Description)
+			x2, err := st.GetEveRace(ctx, 7)
+			if assert.NoError(t, err) {
+				assert.Equal(t, x1, x2)
+			}
+		}
+	})
+	t.Run("should return specific error when race ID is invalid", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		httpmock.Reset()
+		httpmock.RegisterResponder(
+			"GET",
+			"https://esi.evetech.net/v1/universe/races/",
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{
+				{
+					"alliance_id": 500001,
+					"description": "Founded on the tenets of patriotism and hard work...",
+					"name":        "Caldari",
+					"race_id":     7,
+				},
+			}))
+
+		// when
+		_, err := s.GetOrCreateRaceESI(ctx, 42)
+		// then
+		assert.ErrorIs(t, err, app.ErrNotFound)
 	})
 }
