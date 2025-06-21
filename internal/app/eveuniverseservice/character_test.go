c50a91f0cda39966a3059f028608d2be2e3b5c8b
ErikKalkoken <erik.kalkoken@gmail.com>
2025-06-21 20:47:57 +0200

    refactor: eveuniverse package

---
+++ internal/app/eveuniverseservice/character_test.go
@@ -0,0 +1,190 @@
+package eveuniverseservice_test
+
+import (
+	"context"
+	"fmt"
+
+	"testing"
+	"time"
+
+	"github.com/antihax/goesi"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ErikKalkoken/evebuddy/internal/app"
+	"github.com/ErikKalkoken/evebuddy/internal/app/eveuniverseservice"
+	"github.com/ErikKalkoken/evebuddy/internal/app/storage"
+	"github.com/ErikKalkoken/evebuddy/internal/app/storage/testutil"
+)
+
+func TestGetOrCreateEveCharacterESI(t *testing.T) {
+	db, st, factory := testutil.NewDBInMemory()
+	defer db.Close()
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	s := eveuniverseservice.New(eveuniverseservice.Params{
+		Storage:   st,
+		ESIClient: goesi.NewAPIClient(nil, ""),
+	})
+	ctx := context.Background()
+	t.Run("should return existing character", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		httpmock.Reset()
+		c := factory.CreateEveCharacter()
+		// when
+		x1, err := s.GetOrCreateCharacterESI(ctx, c.ID)
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, c.ID, x1.ID)
+		}
+	})
+	t.Run("should fetch minimal character from ESI and create it", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		characterID := int32(95465499)
+		factory.CreateEveEntityCharacter(app.EveEntity{ID: characterID})
+		corporation := factory.CreateEveEntityCorporation(app.EveEntity{ID: 109299958})
+		race := factory.CreateEveRace(app.EveRace{ID: 2})
+		httpmock.Reset()
+		httpmock.RegisterResponder(
+			"GET",
+			fmt.Sprintf("https://esi.evetech.net/v5/characters/%d/", characterID),
+			httpmock.NewJsonResponderOrPanic(200, map[string]any{
+				"birthday":        "2015-03-24T11:37:00Z",
+				"bloodline_id":    3,
+				"corporation_id":  109299958,
+				"gender":          "male",
+				"name":            "CCP Bartender",
+				"race_id":         2,
+				"security_status": -9.9,
+			}))
+		// when
+		x1, err := s.GetOrCreateCharacterESI(ctx, characterID)
+		// then
+		if assert.NoError(t, err) {
+			assert.Nil(t, x1.Alliance)
+			assert.Nil(t, x1.Faction)
+			assert.Equal(t, characterID, x1.ID)
+			assert.Equal(t, time.Date(2015, 03, 24, 11, 37, 0, 0, time.UTC), x1.Birthday)
+			assert.Equal(t, corporation, x1.Corporation)
+			assert.Empty(t, x1.Description)
+			assert.Equal(t, "male", x1.Gender)
+			assert.Equal(t, "CCP Bartender", x1.Name)
+			assert.Equal(t, race, x1.Race)
+			assert.Empty(t, x1.Title)
+			assert.InDelta(t, -9.9, x1.SecurityStatus, 0.01)
+			x2, err := st.GetEveCharacter(ctx, characterID)
+			if assert.NoError(t, err) {
+				assert.Equal(t, x1, x2)
+			}
+		}
+	})
+	t.Run("should fetch full character from ESI and create it", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		characterID := int32(95465499)
+		factory.CreateEveEntityCharacter(app.EveEntity{ID: characterID})
+		alliance := factory.CreateEveEntityCorporation(app.EveEntity{ID: 434243723})
+		corporation := factory.CreateEveEntityCorporation(app.EveEntity{ID: 109299958})
+		faction := factory.CreateEveEntity(app.EveEntity{ID: 500004, Category: app.EveEntityFaction})
+		race := factory.CreateEveRace(app.EveRace{ID: 2})
+		httpmock.Reset()
+		httpmock.RegisterResponder(
+			"GET",
+			fmt.Sprintf("https://esi.evetech.net/v5/characters/%d/", characterID),
+			httpmock.NewJsonResponderOrPanic(200, map[string]any{
+				"birthday":        "2015-03-24T11:37:00Z",
+				"bloodline_id":    3,
+				"alliance_id":     434243723,
+				"corporation_id":  109299958,
+				"faction_id":      500004,
+				"description":     "bla bla",
+				"gender":          "male",
+				"name":            "CCP Bartender",
+				"race_id":         2,
+				"security_status": -9.9,
+				"title":           "All round pretty awesome guy",
+			}))
+		// when
+		x1, err := s.GetOrCreateCharacterESI(ctx, characterID)
+		// then
+		if assert.NoError(t, err) {
+			assert.Equal(t, characterID, x1.ID)
+			assert.Equal(t, time.Date(2015, 03, 24, 11, 37, 0, 0, time.UTC), x1.Birthday)
+			assert.Equal(t, alliance, x1.Alliance)
+			assert.Equal(t, corporation, x1.Corporation)
+			assert.Equal(t, faction, x1.Faction)
+			assert.Equal(t, "bla bla", x1.Description)
+			assert.Equal(t, "male", x1.Gender)
+			assert.Equal(t, "CCP Bartender", x1.Name)
+			assert.Equal(t, race, x1.Race)
+			assert.Equal(t, "All round pretty awesome guy", x1.Title)
+			assert.InDelta(t, -9.9, x1.SecurityStatus, 0.01)
+			x2, err := st.GetEveCharacter(ctx, characterID)
+			if assert.NoError(t, err) {
+				assert.Equal(t, x1, x2)
+			}
+		}
+	})
+}
+
+func TestUpdateAllEveCharactersESI(t *testing.T) {
+	db, st, factory := testutil.NewDBInMemory()
+	defer db.Close()
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	s := eveuniverseservice.NewTestService(st)
+	ctx := context.Background()
+	t.Run("should update character from ESI", func(t *testing.T) {
+		// given
+		testutil.TruncateTables(db)
+		const characterID = 95465499
+		factory.CreateEveEntityCharacter(app.EveEntity{ID: characterID})
+		factory.CreateEveCharacter(storage.CreateEveCharacterParams{ID: characterID})
+		alliance := factory.CreateEveEntityAlliance()
+		corporation := factory.CreateEveEntityCorporation()
+		faction := factory.CreateEveEntity(app.EveEntity{Category: app.EveEntityFaction})
+		httpmock.Reset()
+		httpmock.RegisterResponder(
+			"GET",
+			`=~^https://esi\.evetech\.net/v\d+/characters/\d+/`,
+			httpmock.NewJsonResponderOrPanic(200, map[string]any{
+				"birthday":        "2015-03-24T11:37:00Z",
+				"bloodline_id":    3,
+				"corporation_id":  corporation.ID,
+				"description":     "bla bla",
+				"gender":          "male",
+				"name":            "CCP Bartender",
+				"race_id":         2,
+				"security_status": -9.9,
+				"title":           "All round pretty awesome guy",
+			}),
+		)
+		httpmock.RegisterResponder(
+			"POST",
+			`=~^https://esi\.evetech\.net/v\d+/characters/affiliation/`,
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{
+				{
+					"alliance_id":    alliance.ID,
+					"character_id":   characterID,
+					"corporation_id": corporation.ID,
+					"faction_id":     faction.ID,
+				}}),
+		)
+		// when
+		err := s.UpdateAllCharactersESI(ctx)
+		// then
+		if assert.NoError(t, err) {
+			x, err := st.GetEveCharacter(ctx, characterID)
+			if assert.NoError(t, err) {
+				assert.Equal(t, "CCP Bartender", x.Name)
+				assert.Equal(t, alliance, x.Alliance)
+				assert.Equal(t, corporation, x.Corporation)
+				assert.Equal(t, "bla bla", x.Description)
+				assert.InDelta(t, -9.9, x.SecurityStatus, 0.01)
+				assert.Equal(t, "All round pretty awesome guy", x.Title)
+			}
+		}
+	})
+}
---
+++ internal/app/eveuniverseservice/organization.go
@@ -0,0 +1,260 @@
+package eveuniverseservice
+
+import (
+	"cmp"
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"maps"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/antihax/goesi/esi"
+	"github.com/icrowley/fake"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/ErikKalkoken/evebuddy/internal/app"
+	"github.com/ErikKalkoken/evebuddy/internal/app/storage"
+	"github.com/ErikKalkoken/evebuddy/internal/optional"
+	"github.com/ErikKalkoken/evebuddy/internal/set"
+	"github.com/ErikKalkoken/evebuddy/internal/xiter"
+	"github.com/ErikKalkoken/evebuddy/internal/xslices"
+)
+
+// FetchAlliance fetches an alliance from ESI and returns it.
+func (s *EveUniverseService) FetchAlliance(ctx context.Context, allianceID int32) (*app.EveAlliance, error) {
+	a, _, err := s.esiClient.ESI.AllianceApi.GetAlliancesAllianceId(ctx, allianceID, nil)
+	if err != nil {
+		return nil, err
+	}
+	ids := set.Of(allianceID, a.CreatorCorporationId, a.CreatorId, a.ExecutorCorporationId, a.FactionId)
+	ids.DeleteFunc(func(id int32) bool {
+		return id < 2
+	})
+	eeMap, err := s.ToEntities(ctx, ids)
+	if err != nil {
+		return nil, err
+	}
+	maps.DeleteFunc(eeMap, func(id int32, o *app.EveEntity) bool {
+		return !o.Category.IsKnown()
+	})
+	o := &app.EveAlliance{
+		Creator:             eeMap[a.CreatorId],
+		CreatorCorporation:  eeMap[a.CreatorCorporationId],
+		DateFounded:         a.DateFounded,
+		ExecutorCorporation: eeMap[a.ExecutorCorporationId],
+		Faction:             eeMap[a.FactionId],
+		ID:                  allianceID,
+		Name:                a.Name,
+		Ticker:              a.Ticker,
+	}
+	return o, nil
+}
+
+// FetchAllianceCorporations fetches the corporations for an alliance from ESI and returns them.
+func (s *EveUniverseService) FetchAllianceCorporations(ctx context.Context, allianceID int32) ([]*app.EveEntity, error) {
+	ids, _, err := s.esiClient.ESI.AllianceApi.GetAlliancesAllianceIdCorporations(ctx, allianceID, nil)
+	if err != nil {
+		return nil, err
+	}
+	_, err = s.AddMissingEntities(ctx, set.Union(set.Of(ids...), set.Of(allianceID)))
+	if err != nil {
+		return nil, err
+	}
+	oo, err := s.st.ListEveEntitiesForIDs(ctx, ids)
+	if err != nil {
+		return nil, err
+	}
+	slices.SortFunc(oo, func(a, b *app.EveEntity) int {
+		return strings.Compare(a.Name, b.Name)
+	})
+	return oo, nil
+}
+func (s *EveUniverseService) GetOrCreateCorporationESI(ctx context.Context, id int32) (*app.EveCorporation, error) {
+	o, err := s.st.GetEveCorporation(ctx, id)
+	if errors.Is(err, app.ErrNotFound) {
+		return s.UpdateOrCreateCorporationFromESI(ctx, id)
+	}
+	return o, err
+}
+
+func (s *EveUniverseService) UpdateOrCreateCorporationFromESI(ctx context.Context, id int32) (*app.EveCorporation, error) {
+	x, err, _ := s.sfg.Do(fmt.Sprintf("UpdateOrCreateCorporationFromESI-%d", id), func() (any, error) {
+		r, _, err := s.esiClient.ESI.CorporationApi.GetCorporationsCorporationId(ctx, id, nil)
+		if err != nil {
+			return nil, err
+		}
+		ids := set.Of(id, r.CeoId, r.CreatorId, r.AllianceId, r.FactionId, r.HomeStationId)
+		ids.DeleteFunc(func(id int32) bool {
+			return id < 2
+		})
+		if _, err := s.AddMissingEntities(ctx, ids); err != nil {
+			return nil, err
+		}
+		optionalFromSpecialEntityID := func(v int32) optional.Optional[int32] {
+			if v == 0 || v == 1 {
+				return optional.Optional[int32]{}
+			}
+			return optional.From(v)
+		}
+		arg := storage.UpdateOrCreateEveCorporationParams{
+			AllianceID:    optional.FromIntegerWithZero(r.AllianceId),
+			CeoID:         optionalFromSpecialEntityID(r.CeoId),
+			CreatorID:     optionalFromSpecialEntityID(r.CreatorId),
+			FactionID:     optional.FromIntegerWithZero(r.FactionId),
+			DateFounded:   optional.FromTimeWithZero(r.DateFounded),
+			Description:   r.Description,
+			HomeStationID: optional.FromIntegerWithZero(r.HomeStationId),
+			ID:            id,
+			MemberCount:   r.MemberCount,
+			Name:          r.Name,
+			Shares:        optional.FromIntegerWithZero(r.Shares),
+			TaxRate:       r.TaxRate,
+			Ticker:        r.Ticker,
+			URL:           r.Url,
+			WarEligible:   r.WarEligible,
+		}
+		if err := s.st.UpdateOrCreateEveCorporation(ctx, arg); err != nil {
+			return nil, err
+		}
+		slog.Info("Stored updated eve corporation", "ID", id)
+		return s.st.GetEveCorporation(ctx, id)
+	})
+	if err != nil {
+		return nil, err
+	}
+	return x.(*app.EveCorporation), nil
+}
+
+// UpdateAllCorporationsESI updates all known corporations from ESI.
+func (s *EveUniverseService) UpdateAllCorporationsESI(ctx context.Context) error {
+	ids, err := s.st.ListEveCorporationIDs(ctx)
+	if err != nil {
+		return err
+	}
+	if ids.Size() == 0 {
+		return nil
+	}
+	g := new(errgroup.Group)
+	for id := range ids.All() {
+		g.Go(func() error {
+			_, err := s.UpdateOrCreateCorporationFromESI(ctx, id)
+			return err
+		})
+	}
+	if err := g.Wait(); err != nil {
+		return err
+	}
+	slog.Info("Finished updating eve corporations", "count", ids.Size())
+	return nil
+}
+
+// RandomizeAllCorporationName randomizes the names of all characters.
+func (s *EveUniverseService) RandomizeAllCorporationName(ctx context.Context) error {
+	ids, err := s.st.ListEveCorporationIDs(ctx)
+	if err != nil {
+		return err
+	}
+	if ids.Size() == 0 {
+		return nil
+	}
+	for id := range ids.All() {
+		err := s.st.UpdateEveCorporationName(ctx, id, fake.Company())
+		if err != nil {
+			return err
+		}
+	}
+	return s.scs.UpdateCorporations(ctx)
+}
+
+func (s *EveUniverseService) UpdateShipSkills(ctx context.Context) error {
+	return s.st.UpdateEveShipSkills(ctx)
+}
+
+// FetchCharacterCorporationHistory returns a list of all the corporations a character has been a member of in descending order.
+func (s *EveUniverseService) FetchCharacterCorporationHistory(ctx context.Context, characterID int32) ([]app.MembershipHistoryItem, error) {
+	items, _, err := s.esiClient.ESI.CharacterApi.GetCharactersCharacterIdCorporationhistory(ctx, characterID, nil)
+	if err != nil {
+		return nil, err
+	}
+	items2 := make([]organizationHistoryItem, len(items))
+	for i, it := range items {
+		items2[i] = organizationHistoryItem{
+			OrganizationID: it.CorporationId,
+			IsDeleted:      it.IsDeleted,
+			RecordID:       int(it.RecordId),
+			StartDate:      it.StartDate,
+		}
+	}
+	return s.makeMembershipHistory(ctx, items2)
+}
+
+// FetchCorporationAllianceHistory returns a list of all the alliances a corporation has been a member of in descending order.
+func (s *EveUniverseService) FetchCorporationAllianceHistory(ctx context.Context, corporationID int32) ([]app.MembershipHistoryItem, error) {
+	items, _, err := s.esiClient.ESI.CorporationApi.GetCorporationsCorporationIdAlliancehistory(ctx, corporationID, nil)
+	if err != nil {
+		return nil, err
+	}
+	items2 := xslices.Map(items, func(x esi.GetCorporationsCorporationIdAlliancehistory200Ok) organizationHistoryItem {
+		return organizationHistoryItem{
+			OrganizationID: x.AllianceId,
+			IsDeleted:      x.IsDeleted,
+			RecordID:       int(x.RecordId),
+			StartDate:      x.StartDate,
+		}
+	})
+	return s.makeMembershipHistory(ctx, items2)
+}
+
+type organizationHistoryItem struct {
+	OrganizationID int32
+	IsDeleted      bool
+	RecordID       int
+	StartDate      time.Time
+}
+
+func (s *EveUniverseService) makeMembershipHistory(ctx context.Context, items []organizationHistoryItem) ([]app.MembershipHistoryItem, error) {
+	ids := set.Collect(xiter.Map(slices.Values(items), func(x organizationHistoryItem) int32 {
+		return x.OrganizationID
+	}))
+	ids.DeleteFunc(func(id int32) bool {
+		return id < 2
+	})
+	eeMap, err := s.ToEntities(ctx, ids)
+	if err != nil {
+		return nil, err
+	}
+	slices.SortFunc(items, func(a, b organizationHistoryItem) int {
+		return cmp.Compare(a.RecordID, b.RecordID)
+	})
+
+	oo := make([]app.MembershipHistoryItem, len(items))
+	for i, it := range items {
+		var endDate time.Time
+		if i+1 < len(items) {
+			endDate = items[i+1].StartDate
+		}
+		var endDate2 time.Time
+		if !endDate.IsZero() {
+			endDate2 = endDate
+		} else {
+			endDate2 = s.Now()
+		}
+		days := int(endDate2.Sub(it.StartDate) / (time.Hour * 24))
+		oo[i] = app.MembershipHistoryItem{
+			Days:         days,
+			EndDate:      endDate,
+			IsDeleted:    it.IsDeleted,
+			IsOldest:     i == 0,
+			RecordID:     it.RecordID,
+			StartDate:    it.StartDate,
+			Organization: eeMap[it.OrganizationID],
+		}
+	}
+	slices.SortFunc(oo, func(a, b app.MembershipHistoryItem) int {
+		return -cmp.Compare(a.RecordID, b.RecordID)
+	})
+	return oo, nil
+}
