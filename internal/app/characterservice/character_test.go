a96988aeec3020f991be3267846460edea7af9ce
ErikKalkoken <erik.kalkoken@gmail.com>
2025-07-21 12:35:49 +0200

    refactor: character service test files

--- internal/app/characterservice/character_test.go
+++ internal/app/characterservice/character_test.go
@@ -10,11 +10,9 @@ import (
 
 	"github.com/ErikKalkoken/evebuddy/internal/app"
 	"github.com/ErikKalkoken/evebuddy/internal/app/characterservice"
-	"github.com/ErikKalkoken/evebuddy/internal/app/evenotification"
 	"github.com/ErikKalkoken/evebuddy/internal/app/storage"
 	"github.com/ErikKalkoken/evebuddy/internal/app/storage/testutil"
 	"github.com/ErikKalkoken/evebuddy/internal/optional"
-	"github.com/ErikKalkoken/evebuddy/internal/set"
 )
 
 func TestGetCharacter(t *testing.T) {
@@ -287,82 +285,6 @@ func TestNotifyUpdatedContracts(t *testing.T) {
 				assert.Equal(t, tc.shouldNotify, sendCount == 1)
 			}
 		})
-	}
-}
-
-func TestNotifyCommunications(t *testing.T) {
-	db, st, factory := testutil.NewDBInMemory()
-	defer db.Close()
-	cs := characterservice.NewFake(st)
-	ctx := context.Background()
-	now := time.Now().UTC()
-	earliest := now.Add(-12 * time.Hour)
-	typesEnabled := set.Of(string(evenotification.StructureUnderAttack))
-	cases := []struct {
-		name         string
-		typ          evenotification.Type
-		timestamp    time.Time
-		isProcessed  bool
-		shouldNotify bool
-	}{
-		{"send unprocessed", evenotification.StructureUnderAttack, now, false, true},
-		{"don't send old unprocessed", evenotification.StructureUnderAttack, now.Add(-16 * time.Hour), false, false},
-		{"don't send not enabled types", evenotification.SkyhookOnline, now, false, false},
-		{"don't resend already processed", evenotification.StructureUnderAttack, now, true, false},
-	}
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			// given
-			testutil.TruncateTables(db)
-			n := factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
-				IsProcessed: tc.isProcessed,
-				Title:       optional.New("title"),
-				Body:        optional.New("body"),
-				Type:        string(tc.typ),
-				Timestamp:   tc.timestamp,
-			})
-			var sendCount int
-			// when
-			err := cs.NotifyCommunications(ctx, n.CharacterID, earliest, typesEnabled, func(title string, content string) {
-				sendCount++
-			})
-			// then
-			if assert.NoError(t, err) {
-				assert.Equal(t, tc.shouldNotify, sendCount == 1)
-			}
-		})
-	}
-}
-
-func TestCountNotifications(t *testing.T) {
-	db, st, factory := testutil.NewDBInMemory()
-	defer db.Close()
-	// given
-	cs := characterservice.NewFake(st)
-	ctx := context.Background()
-	c := factory.CreateCharacterFull()
-	factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
-		CharacterID: c.ID,
-		Type:        string(evenotification.StructureDestroyed),
-	})
-	factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
-		CharacterID: c.ID,
-		Type:        string(evenotification.MoonminingExtractionStarted),
-	})
-	factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
-		CharacterID: c.ID,
-		Type:        string(evenotification.MoonminingExtractionStarted),
-		IsRead:      true,
-	})
-	factory.CreateCharacterNotification()
-	// when
-	got, err := cs.CountNotifications(ctx, c.ID)
-	if assert.NoError(t, err) {
-		want := map[app.NotificationGroup][]int{
-			app.GroupStructure:  {1, 1},
-			app.GroupMoonMining: {2, 1},
-		}
-		assert.Equal(t, want, got)
 	}
 }
 
--- internal/app/characterservice/communications_test.go
+++ internal/app/characterservice/communications_test.go
@@ -1,181 +1,92 @@
-package characterservice
+package characterservice_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
-	"github.com/jarcoal/httpmock"
-	"github.com/stretchr/testify/assert"
-
 	"github.com/ErikKalkoken/evebuddy/internal/app"
+	"github.com/ErikKalkoken/evebuddy/internal/app/characterservice"
 	"github.com/ErikKalkoken/evebuddy/internal/app/evenotification"
 	"github.com/ErikKalkoken/evebuddy/internal/app/storage"
 	"github.com/ErikKalkoken/evebuddy/internal/app/storage/testutil"
+	"github.com/ErikKalkoken/evebuddy/internal/optional"
+	"github.com/ErikKalkoken/evebuddy/internal/set"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestUpdateCharacterNotificationsESI(t *testing.T) {
-	db, st, factory := testutil.NewDBOnDisk(t)
+func TestNotifyCommunications(t *testing.T) {
+	db, st, factory := testutil.NewDBInMemory()
 	defer db.Close()
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-	s := NewFake(st)
+	cs := characterservice.NewFake(st)
 	ctx := context.Background()
-	t.Run("should create new notification from scratch", func(t *testing.T) {
-		// given
-		testutil.TruncateTables(db)
-		httpmock.Reset()
-		c := factory.CreateCharacterFull()
-		factory.CreateCharacterToken(storage.UpdateOrCreateCharacterTokenParams{CharacterID: c.ID})
-		sender := factory.CreateEveEntityCorporation(app.EveEntity{ID: 54321})
-		data := []map[string]any{{
-			"is_read":         true,
-			"notification_id": 42,
-			"sender_id":       sender.ID,
-			"sender_type":     "corporation",
-			"text":            "amount: 3731016.4000000004\\nitemID: 1024881021663\\npayout: 1\\n",
-			"timestamp":       "2017-08-16T10:08:00Z",
-			"type":            "InsurancePayoutMsg"}}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v4/characters/%d/notifications/", c.ID),
-			httpmock.NewJsonResponderOrPanic(200, data))
-		// when
-		changed, err := s.updateNotificationsESI(ctx, app.CharacterUpdateSectionParams{
-			CharacterID: c.ID,
-			Section:     app.SectionCharacterNotifications,
+	now := time.Now().UTC()
+	earliest := now.Add(-12 * time.Hour)
+	typesEnabled := set.Of(string(evenotification.StructureUnderAttack))
+	cases := []struct {
+		name         string
+		typ          evenotification.Type
+		timestamp    time.Time
+		isProcessed  bool
+		shouldNotify bool
+	}{
+		{"send unprocessed", evenotification.StructureUnderAttack, now, false, true},
+		{"don't send old unprocessed", evenotification.StructureUnderAttack, now.Add(-16 * time.Hour), false, false},
+		{"don't send not enabled types", evenotification.SkyhookOnline, now, false, false},
+		{"don't resend already processed", evenotification.StructureUnderAttack, now, true, false},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			// given
+			testutil.TruncateTables(db)
+			n := factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
+				IsProcessed: tc.isProcessed,
+				Title:       optional.New("title"),
+				Body:        optional.New("body"),
+				Type:        string(tc.typ),
+				Timestamp:   tc.timestamp,
+			})
+			var sendCount int
+			// when
+			err := cs.NotifyCommunications(ctx, n.CharacterID, earliest, typesEnabled, func(title string, content string) {
+				sendCount++
+			})
+			// then
+			if assert.NoError(t, err) {
+				assert.Equal(t, tc.shouldNotify, sendCount == 1)
+			}
 		})
-		// then
-		if assert.NoError(t, err) {
-			assert.True(t, changed)
-			o, err := st.GetCharacterNotification(ctx, c.ID, 42)
-			if assert.NoError(t, err) {
-				assert.True(t, o.IsRead)
-				assert.Equal(t, int64(42), o.NotificationID)
-				assert.Equal(t, sender, o.Sender)
-				assert.Equal(t, "InsurancePayoutMsg", o.Type)
-				assert.Equal(t, "amount: 3731016.4000000004\\nitemID: 1024881021663\\npayout: 1\\n", o.Text)
-				assert.Equal(t, time.Date(2017, 8, 16, 10, 8, 0, 0, time.UTC), o.Timestamp)
-			}
-			ids, err := st.ListCharacterNotificationIDs(ctx, c.ID)
-			if assert.NoError(t, err) {
-				assert.Equal(t, 1, ids.Size())
-			}
-		}
-	})
-	t.Run("should add new notification", func(t *testing.T) {
-		// given
-		testutil.TruncateTables(db)
-		httpmock.Reset()
-		c := factory.CreateCharacterFull()
-		factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{CharacterID: c.ID})
-		factory.CreateCharacterToken(storage.UpdateOrCreateCharacterTokenParams{CharacterID: c.ID})
-		sender := factory.CreateEveEntityCorporation(app.EveEntity{ID: 54321})
-		data := []map[string]any{{
-			"is_read":         true,
-			"notification_id": 42,
-			"sender_id":       sender.ID,
-			"sender_type":     "corporation",
-			"text":            "amount: 3731016.4000000004\\nitemID: 1024881021663\\npayout: 1\\n",
-			"timestamp":       "2017-08-16T10:08:00Z",
-			"type":            "InsurancePayoutMsg"}}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v4/characters/%d/notifications/", c.ID),
-			httpmock.NewJsonResponderOrPanic(200, data))
-		// when
-		changed, err := s.updateNotificationsESI(ctx, app.CharacterUpdateSectionParams{
-			CharacterID: c.ID,
-			Section:     app.SectionCharacterNotifications,
-		})
-		// then
-		if assert.NoError(t, err) {
-			assert.True(t, changed)
-			o, err := st.GetCharacterNotification(ctx, c.ID, 42)
-			if assert.NoError(t, err) {
-				assert.True(t, o.IsRead)
-				assert.Equal(t, int64(42), o.NotificationID)
-				assert.Equal(t, sender, o.Sender)
-				assert.Equal(t, "InsurancePayoutMsg", o.Type)
-				assert.Equal(t, "amount: 3731016.4000000004\\nitemID: 1024881021663\\npayout: 1\\n", o.Text)
-				assert.Equal(t, time.Date(2017, 8, 16, 10, 8, 0, 0, time.UTC), o.Timestamp)
-			}
-			ids, err := st.ListCharacterNotificationIDs(ctx, c.ID)
-			if assert.NoError(t, err) {
-				assert.Equal(t, 2, ids.Size())
-			}
-		}
-	})
-	t.Run("should update isRead for existing notification", func(t *testing.T) {
-		// given
-		testutil.TruncateTables(db)
-		httpmock.Reset()
-		c := factory.CreateCharacterFull()
-		factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
-			CharacterID:    c.ID,
-			NotificationID: 42,
-		})
-		factory.CreateCharacterToken(storage.UpdateOrCreateCharacterTokenParams{CharacterID: c.ID})
-		sender := factory.CreateEveEntityCorporation(app.EveEntity{ID: 54321})
-		data := []map[string]any{{
-			"is_read":         true,
-			"notification_id": 42,
-			"sender_id":       sender.ID,
-			"sender_type":     "corporation",
-			"text":            "amount: 3731016.4000000004\\nitemID: 1024881021663\\npayout: 1\\n",
-			"timestamp":       "2017-08-16T10:08:00Z",
-			"type":            "InsurancePayoutMsg"}}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v4/characters/%d/notifications/", c.ID),
-			httpmock.NewJsonResponderOrPanic(200, data))
-		// when
-		changed, err := s.updateNotificationsESI(ctx, app.CharacterUpdateSectionParams{
-			CharacterID: c.ID,
-			Section:     app.SectionCharacterNotifications,
-		})
-		// then
-		if assert.NoError(t, err) {
-			assert.True(t, changed)
-			o, err := st.GetCharacterNotification(ctx, c.ID, 42)
-			if assert.NoError(t, err) {
-				assert.True(t, o.IsRead)
-			}
-			ids, err := st.ListCharacterNotificationIDs(ctx, c.ID)
-			if assert.NoError(t, err) {
-				assert.Equal(t, 1, ids.Size())
-			}
-		}
-	})
+	}
 }
 
-func TestListCharacterNotifications(t *testing.T) {
-	db, st, factory := testutil.NewDBOnDisk(t)
+func TestCountNotifications(t *testing.T) {
+	db, st, factory := testutil.NewDBInMemory()
 	defer db.Close()
-	s := NewFake(st)
+	// given
+	cs := characterservice.NewFake(st)
 	ctx := context.Background()
-	t.Run("can list existing entries", func(t *testing.T) {
-		// given
-		testutil.TruncateTables(db)
-		c := factory.CreateCharacterFull()
-		factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
-			CharacterID: c.ID,
-			Type:        string(evenotification.BillOutOfMoneyMsg),
-		})
-		factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
-			CharacterID: c.ID,
-			Type:        string(evenotification.BillPaidCorpAllMsg),
-		})
-		factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
-			CharacterID: c.ID,
-			Type:        "alpha",
-		})
-		// when
-		tt, err := s.ListNotificationsTypes(ctx, c.ID, app.GroupBills)
-		// then
-		if assert.NoError(t, err) {
-			assert.Len(t, tt, 2)
-		}
+	c := factory.CreateCharacterFull()
+	factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
+		CharacterID: c.ID,
+		Type:        string(evenotification.StructureDestroyed),
 	})
+	factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
+		CharacterID: c.ID,
+		Type:        string(evenotification.MoonminingExtractionStarted),
+	})
+	factory.CreateCharacterNotification(storage.CreateCharacterNotificationParams{
+		CharacterID: c.ID,
+		Type:        string(evenotification.MoonminingExtractionStarted),
+		IsRead:      true,
+	})
+	factory.CreateCharacterNotification()
+	// when
+	got, err := cs.CountNotifications(ctx, c.ID)
+	if assert.NoError(t, err) {
+		want := map[app.NotificationGroup][]int{
+			app.GroupStructure:  {1, 1},
+			app.GroupMoonMining: {2, 1},
+		}
+		assert.Equal(t, want, got)
+	}
 }
