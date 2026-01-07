4f0bf7bbbf35e6e62bd2d0956d1d2559052624ef
ErikKalkoken <erik.kalkoken@gmail.com>
2026-01-07 16:51:04 +0100

    refactor: consolidate icons package

--- internal/app/icons/generate.go
+++ internal/app/icons/generate.go
@@ -1,3 +1,4 @@
+// Package icons contains all bundeled app icons.
 package icons
 
-//go:generate go tool fyne bundle -o resource_gen.go -pkg icons --prefix "" resources
+//go:generate go tool fyne bundle -o resource_gen.go -pkg icons --prefix "" .
