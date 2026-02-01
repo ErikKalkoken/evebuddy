3520119d5aa532e4035590a678d36ca9e48aed4e
ErikKalkoken <erik.kalkoken@gmail.com>
2026-02-01 14:11:06 +0100

    feat: add generic sync map

---
+++ internal/xsync/xsync.go
@@ -0,0 +1,47 @@
+// Package xsync extends the standard sync package.
+package xsync
+
+import (
+	"sync"
+)
+
+// Map is a type-safe concurrent map.
+type Map[K comparable, V any] struct {
+	m sync.Map
+}
+
+// Store sets the value for a key.
+func (m *Map[K, V]) Store(key K, value V) {
+	m.m.Store(key, value)
+}
+
+// Load returns the value stored in the map for a key, or nil if no value is present.
+// The ok result indicates whether value was found in the map.
+func (m *Map[K, V]) Load(key K) (V, bool) {
+	v, ok := m.m.Load(key)
+	if !ok {
+		var zero V
+		return zero, false
+	}
+	return v.(V), true
+}
+
+// LoadOrStore returns the existing value for the key if present.
+// Otherwise, it stores and returns the given value.
+func (m *Map[K, V]) LoadOrStore(key K, value V) (V, bool) {
+	v, loaded := m.m.LoadOrStore(key, value)
+	return v.(V), loaded
+}
+
+// Delete deletes the value for a key.
+func (m *Map[K, V]) Delete(key K) {
+	m.m.Delete(key)
+}
+
+// Range calls f sequentially for each key and value present in the map.
+// If f returns false, range stops the iteration.
+func (m *Map[K, V]) Range(f func(key K, value V) bool) {
+	m.m.Range(func(k, v any) bool {
+		return f(k.(K), v.(V))
+	})
+}
