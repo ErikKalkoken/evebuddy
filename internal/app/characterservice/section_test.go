25e355cf05d2aeac9970cbb622d2e2ada6fdf8f9
ErikKalkoken <erik.kalkoken@gmail.com>
2026-02-06 17:07:57 +0100

    fix: HasSection not reporting on missing sections

--- internal/app/characterservice/section_test.go
+++ internal/app/characterservice/section_test.go
@@ -2,19 +2,77 @@ package characterservice_test
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
 	"time"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/ErikKalkoken/evebuddy/internal/app"
 	"github.com/ErikKalkoken/evebuddy/internal/app/characterservice"
 	"github.com/ErikKalkoken/evebuddy/internal/app/storage"
 	"github.com/ErikKalkoken/evebuddy/internal/app/testutil"
-	"github.com/jarcoal/httpmock"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestUpdateCharacterSection(t *testing.T) {
+func TestCharacterService_HasSection(t *testing.T) {
+	db, st, factory := testutil.NewDBInMemory()
+	defer db.Close()
+	s := characterservice.NewFake(st)
+	section := app.SectionCharacterImplants
+	ctx := context.Background()
+	t.Run("should report true when exists", func(t *testing.T) {
+		// given
+		testutil.MustTruncateTables(db)
+		httpmock.Reset()
+		c := factory.CreateCharacter()
+		factory.CreateCharacterSectionStatus(testutil.CharacterSectionStatusParams{
+			CharacterID: c.ID,
+			Section:     section,
+		})
+		// when
+		got, err := s.HasSection(ctx, c.ID, section)
+		// then
+		require.NoError(t, err)
+		assert.True(t, got)
+	})
+	t.Run("should report false when not exists", func(t *testing.T) {
+		// given
+		testutil.MustTruncateTables(db)
+		httpmock.Reset()
+		c := factory.CreateCharacter()
+		// when
+		got, err := s.HasSection(ctx, c.ID, section)
+		// then
+		require.NoError(t, err)
+		assert.False(t, got)
+	})
+	t.Run("should report false when exist, but incomplete", func(t *testing.T) {
+		// given
+		testutil.MustTruncateTables(db)
+		httpmock.Reset()
+		c := factory.CreateCharacter()
+		factory.CreateCharacterSectionStatus(testutil.CharacterSectionStatusParams{
+			CharacterID: c.ID,
+			Section:     section,
+		})
+		_, err := st.UpdateOrCreateCharacterSectionStatus(ctx, storage.UpdateOrCreateCharacterSectionStatusParams{
+			CharacterID: c.ID,
+			Section:     section,
+			CompletedAt: &sql.NullTime{},
+		})
+		require.NoError(t, err)
+		// when
+		got, err := s.HasSection(ctx, c.ID, section)
+		// then
+		require.NoError(t, err)
+		assert.False(t, got)
+	})
+}
+
+func TestCharacterService_UpdateSectionIfNeeded(t *testing.T) {
 	db, st, factory := testutil.NewDBInMemory()
 	defer db.Close()
 	httpmock.Activate()
@@ -34,16 +92,16 @@ func TestUpdateCharacterSection(t *testing.T) {
 			fmt.Sprintf("https://esi.evetech.net/v1/characters/%d/implants/", c.ID),
 			httpmock.NewJsonResponderOrPanic(200, []int32{et.ID}))
 		// when
-		changed, err := s.UpdateSectionIfNeeded(
-			ctx, app.CharacterSectionUpdateParams{CharacterID: c.ID, Section: section})
+		changed, err := s.UpdateSectionIfNeeded(ctx, app.CharacterSectionUpdateParams{
+			CharacterID: c.ID,
+			Section:     section,
+		})
 		// then
-		if assert.NoError(t, err) {
-			assert.True(t, changed)
-			x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
-			if assert.NoError(t, err) {
-				assert.False(t, x.HasError())
-			}
-		}
+		require.NoError(t, err)
+		assert.True(t, changed)
+		x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
+		require.NoError(t, err)
+		assert.False(t, x.HasError())
 	})
 	t.Run("should not update and report false when not changed", func(t *testing.T) {
 		// given
@@ -63,21 +121,21 @@ func TestUpdateCharacterSection(t *testing.T) {
 			fmt.Sprintf("https://esi.evetech.net/v1/characters/%d/implants/", c.ID),
 			httpmock.NewJsonResponderOrPanic(200, data))
 		// when
-		changed, err := s.UpdateSectionIfNeeded(
-			ctx, app.CharacterSectionUpdateParams{CharacterID: c.ID, Section: section})
+		changed, err := s.UpdateSectionIfNeeded(ctx, app.CharacterSectionUpdateParams{
+			CharacterID: c.ID,
+			Section:     section,
+		})
 		// then
-		if assert.NoError(t, err) {
-			assert.False(t, changed)
-			x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
-			if assert.NoError(t, err) {
-				assert.WithinDuration(t, time.Now(), x.CompletedAt, 5*time.Second)
-			}
-			assert.Equal(t, 1, httpmock.GetTotalCallCount())
-			xx, err := st.ListCharacterImplants(ctx, c.ID)
-			if assert.NoError(t, err) {
-				assert.Len(t, xx, 0)
-			}
-		}
+		require.NoError(t, err)
+		assert.False(t, changed)
+		x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
+		require.NoError(t, err)
+		assert.WithinDuration(t, time.Now(), x.CompletedAt, 5*time.Second)
+
+		assert.Equal(t, 1, httpmock.GetTotalCallCount())
+		xx, err := st.ListCharacterImplants(ctx, c.ID)
+		require.NoError(t, err)
+		assert.Len(t, xx, 0)
 	})
 	t.Run("should not fetch or update when not expired and report false", func(t *testing.T) {
 		// given
@@ -95,20 +153,17 @@ func TestUpdateCharacterSection(t *testing.T) {
 			fmt.Sprintf("https://esi.evetech.net/v1/characters/%d/implants/", c.ID),
 			httpmock.NewJsonResponderOrPanic(200, []int32{et.ID}))
 		// when
-		changed, err := s.UpdateSectionIfNeeded(
-			ctx, app.CharacterSectionUpdateParams{
-				CharacterID: c.ID,
-				Section:     section,
-			})
+		changed, err := s.UpdateSectionIfNeeded(ctx, app.CharacterSectionUpdateParams{
+			CharacterID: c.ID,
+			Section:     section,
+		})
 		// then
-		if assert.NoError(t, err) {
-			assert.False(t, changed)
-			assert.Equal(t, 0, httpmock.GetTotalCallCount())
-			xx, err := st.ListCharacterImplants(ctx, c.ID)
-			if assert.NoError(t, err) {
-				assert.Len(t, xx, 0)
-			}
-		}
+		require.NoError(t, err)
+		assert.False(t, changed)
+		assert.Equal(t, 0, httpmock.GetTotalCallCount())
+		xx, err := st.ListCharacterImplants(ctx, c.ID)
+		require.NoError(t, err)
+		assert.Len(t, xx, 0)
 	})
 	t.Run("should record when update failed", func(t *testing.T) {
 		// given
@@ -121,16 +176,16 @@ func TestUpdateCharacterSection(t *testing.T) {
 			fmt.Sprintf("https://esi.evetech.net/v1/characters/%d/implants/", c.ID),
 			httpmock.NewJsonResponderOrPanic(500, map[string]string{"error": "dummy error"}))
 		// when
-		_, err := s.UpdateSectionIfNeeded(
-			ctx, app.CharacterSectionUpdateParams{CharacterID: c.ID, Section: section})
+		_, err := s.UpdateSectionIfNeeded(ctx, app.CharacterSectionUpdateParams{
+			CharacterID: c.ID,
+			Section:     section,
+		})
 		// then
-		if assert.Error(t, err) {
-			x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
-			if assert.NoError(t, err) {
-				assert.True(t, x.HasError())
-				assert.Equal(t, "500 Internal Server Error", x.ErrorMessage)
-			}
-		}
+		require.Error(t, err)
+		x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
+		require.NoError(t, err)
+		assert.True(t, x.HasError())
+		assert.Equal(t, "500 Internal Server Error", x.ErrorMessage)
 	})
 	t.Run("should fetch and update when not expired and force update requested", func(t *testing.T) {
 		// given
@@ -148,20 +203,17 @@ func TestUpdateCharacterSection(t *testing.T) {
 			fmt.Sprintf("https://esi.evetech.net/v1/characters/%d/implants/", c.ID),
 			httpmock.NewJsonResponderOrPanic(200, []int32{et.ID}))
 		// when
-		_, err := s.UpdateSectionIfNeeded(
-			ctx, app.CharacterSectionUpdateParams{
-				CharacterID: c.ID,
-				Section:     section,
-				ForceUpdate: true,
-			})
+		_, err := s.UpdateSectionIfNeeded(ctx, app.CharacterSectionUpdateParams{
+			CharacterID: c.ID,
+			Section:     section,
+			ForceUpdate: true,
+		})
 		// then
-		if assert.NoError(t, err) {
-			assert.Equal(t, 1, httpmock.GetTotalCallCount())
-			xx, err := st.ListCharacterImplants(ctx, c.ID)
-			if assert.NoError(t, err) {
-				assert.Len(t, xx, 1)
-			}
-		}
+		require.NoError(t, err)
+		assert.Equal(t, 1, httpmock.GetTotalCallCount())
+		xx, err := st.ListCharacterImplants(ctx, c.ID)
+		require.NoError(t, err)
+		assert.Len(t, xx, 1)
 	})
 	t.Run("should update when not changed and force update requested", func(t *testing.T) {
 		// given
@@ -182,24 +234,20 @@ func TestUpdateCharacterSection(t *testing.T) {
 			fmt.Sprintf("https://esi.evetech.net/v1/characters/%d/implants/", c.ID),
 			httpmock.NewJsonResponderOrPanic(200, data))
 		// when
-		_, err := s.UpdateSectionIfNeeded(
-			ctx, app.CharacterSectionUpdateParams{
-				CharacterID: c.ID,
-				Section:     section,
-				ForceUpdate: true,
-			})
+		_, err := s.UpdateSectionIfNeeded(ctx, app.CharacterSectionUpdateParams{
+			CharacterID: c.ID,
+			Section:     section,
+			ForceUpdate: true,
+		})
 		// then
-		if assert.NoError(t, err) {
-			x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
-			if assert.NoError(t, err) {
-				assert.WithinDuration(t, time.Now(), x.CompletedAt, 5*time.Second)
-			}
-			assert.Equal(t, 1, httpmock.GetTotalCallCount())
-			xx, err := st.ListCharacterImplants(ctx, c.ID)
-			if assert.NoError(t, err) {
-				assert.Len(t, xx, 1)
-			}
-		}
+		require.NoError(t, err)
+		x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
+		require.NoError(t, err)
+		assert.WithinDuration(t, time.Now(), x.CompletedAt, 5*time.Second)
+		assert.Equal(t, 1, httpmock.GetTotalCallCount())
+		xx, err := st.ListCharacterImplants(ctx, c.ID)
+		require.NoError(t, err)
+		assert.Len(t, xx, 1)
 	})
 	t.Run("should update when last update failed and error has timed out", func(t *testing.T) {
 		// given
@@ -222,23 +270,19 @@ func TestUpdateCharacterSection(t *testing.T) {
 			fmt.Sprintf("https://esi.evetech.net/v1/characters/%d/implants/", c.ID),
 			httpmock.NewJsonResponderOrPanic(200, data))
 		// when
-		_, err := s.UpdateSectionIfNeeded(
-			ctx, app.CharacterSectionUpdateParams{
-				CharacterID: c.ID,
-				Section:     section,
-			})
+		_, err := s.UpdateSectionIfNeeded(ctx, app.CharacterSectionUpdateParams{
+			CharacterID: c.ID,
+			Section:     section,
+		})
 		// then
-		if assert.NoError(t, err) {
-			x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
-			if assert.NoError(t, err) {
-				assert.WithinDuration(t, time.Now(), x.CompletedAt, 5*time.Second)
-			}
-			assert.Equal(t, 1, httpmock.GetTotalCallCount())
-			xx, err := st.ListCharacterImplants(ctx, c.ID)
-			if assert.NoError(t, err) {
-				assert.Len(t, xx, 1)
-			}
-		}
+		require.NoError(t, err)
+		x, err := st.GetCharacterSectionStatus(ctx, c.ID, section)
+		require.NoError(t, err)
+		assert.WithinDuration(t, time.Now(), x.CompletedAt, 5*time.Second)
+		assert.Equal(t, 1, httpmock.GetTotalCallCount())
+		xx, err := st.ListCharacterImplants(ctx, c.ID)
+		require.NoError(t, err)
+		assert.Len(t, xx, 1)
 	})
 	t.Run("should not update when last update failed but below error timeout", func(t *testing.T) {
 		// given
@@ -261,19 +305,16 @@ func TestUpdateCharacterSection(t *testing.T) {
 			fmt.Sprintf("https://esi.evetech.net/v1/characters/%d/implants/", c.ID),
 			httpmock.NewJsonResponderOrPanic(200, data))
 		// when
-		changed, err := s.UpdateSectionIfNeeded(
-			ctx, app.CharacterSectionUpdateParams{
-				CharacterID: c.ID,
-				Section:     section,
-			})
+		changed, err := s.UpdateSectionIfNeeded(ctx, app.CharacterSectionUpdateParams{
+			CharacterID: c.ID,
+			Section:     section,
+		})
 		// then
-		if assert.NoError(t, err) {
-			assert.False(t, changed)
-			assert.Equal(t, 0, httpmock.GetTotalCallCount())
-			xx, err := st.ListCharacterImplants(ctx, c.ID)
-			if assert.NoError(t, err) {
-				assert.Len(t, xx, 0)
-			}
-		}
+		require.NoError(t, err)
+		assert.False(t, changed)
+		assert.Equal(t, 0, httpmock.GetTotalCallCount())
+		xx, err := st.ListCharacterImplants(ctx, c.ID)
+		require.NoError(t, err)
+		assert.Len(t, xx, 0)
 	})
 }
