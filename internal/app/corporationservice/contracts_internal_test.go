a4e364586adfca8d13eed765129428e0fed651b4
ErikKalkoken <erik.kalkoken@gmail.com>
2026-02-07 14:13:39 +0100

    refactor: corp service tests

--- internal/app/corporationservice/contracts_internal_test.go
+++ internal/app/corporationservice/contracts_internal_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/ErikKalkoken/evebuddy/internal/app"
 	"github.com/ErikKalkoken/evebuddy/internal/app/storage"
@@ -33,8 +34,10 @@ func TestUpdateContractESI(t *testing.T) {
 		startLocation := factory.CreateEveLocationStructure()
 		endLocation := factory.CreateEveLocationStructure()
 		volume := 0.01
-		contractData := []map[string]any{
-			{
+		httpmock.RegisterResponder(
+			"GET",
+			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/", c.ID),
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{{
 				"acceptor_id":           0,
 				"assignee_id":           0,
 				"availability":          "public",
@@ -54,29 +57,21 @@ func TestUpdateContractESI(t *testing.T) {
 				"status":                "finished",
 				"type":                  "courier",
 				"volume":                volume,
-			},
-		}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/", c.ID),
-			httpmock.NewJsonResponderOrPanic(200, contractData),
+			}}),
 		)
 		recordID := int64(123456)
 		quantity := 7
 		et := factory.CreateEveType()
-		itemData := []map[string]any{
-			{
+		httpmock.RegisterResponder(
+			"GET",
+			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/%d/items/", c.ID, contractID),
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{{
 				"is_included":  true,
 				"is_singleton": false,
 				"quantity":     quantity,
 				"record_id":    recordID,
 				"type_id":      et.ID,
-			},
-		}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/%d/items/", c.ID, contractID),
-			httpmock.NewJsonResponderOrPanic(200, itemData),
+			}}),
 		)
 		// when
 		changed, err := s.updateContractsESI(ctx, app.CorporationSectionUpdateParams{
@@ -84,27 +79,27 @@ func TestUpdateContractESI(t *testing.T) {
 			Section:       app.SectionCorporationContracts,
 		})
 		// then
-		if assert.NoError(t, err) {
-			assert.True(t, changed)
-			o, err := st.GetCorporationContract(ctx, c.ID, contractID)
-			if assert.NoError(t, err) {
-				assert.Equal(t, time.Date(2017, 6, 6, 13, 12, 32, 0, time.UTC), o.DateAccepted.MustValue())
-				assert.Equal(t, time.Date(2017, 6, 7, 13, 12, 32, 0, time.UTC), o.DateCompleted.MustValue())
-				assert.Equal(t, time.Date(2017, 6, 13, 13, 12, 32, 0, time.UTC), o.DateExpired)
-				assert.Equal(t, time.Date(2017, 6, 5, 13, 12, 32, 0, time.UTC), o.DateIssued)
-				assert.Equal(t, startLocation.ID, o.StartLocation.ID)
-				assert.Equal(t, endLocation.ID, o.EndLocation.ID)
-				assert.Equal(t, app.ContractStatusFinished, o.Status)
-				assert.Equal(t, app.ContractTypeCourier, o.Type)
-				assert.Equal(t, volume, o.Volume)
-			}
-			ii, err := st.ListCorporationContractItems(ctx, o.ID)
-			if assert.NoError(t, err) && assert.Len(t, ii, 1) {
-				assert.Equal(t, quantity, ii[0].Quantity)
-				assert.Equal(t, recordID, ii[0].RecordID)
-				assert.Equal(t, et, ii[0].Type)
-			}
-		}
+		require.NoError(t, err)
+		assert.True(t, changed)
+		o, err := st.GetCorporationContract(ctx, c.ID, contractID)
+		require.NoError(t, err)
+		assert.Equal(t, time.Date(2017, 6, 6, 13, 12, 32, 0, time.UTC), o.DateAccepted.MustValue())
+		assert.Equal(t, time.Date(2017, 6, 7, 13, 12, 32, 0, time.UTC), o.DateCompleted.MustValue())
+		assert.Equal(t, time.Date(2017, 6, 13, 13, 12, 32, 0, time.UTC), o.DateExpired)
+		assert.Equal(t, time.Date(2017, 6, 5, 13, 12, 32, 0, time.UTC), o.DateIssued)
+		assert.Equal(t, startLocation.ID, o.StartLocation.ID)
+		assert.Equal(t, endLocation.ID, o.EndLocation.ID)
+		assert.Equal(t, app.ContractStatusFinished, o.Status)
+		assert.Equal(t, app.ContractTypeCourier, o.Type)
+		assert.Equal(t, volume, o.Volume)
+		ii, err := st.ListCorporationContractItems(ctx, o.ID)
+
+		require.NoError(t, err)
+		require.Len(t, ii, 1)
+		assert.Equal(t, quantity, ii[0].Quantity)
+		assert.Equal(t, recordID, ii[0].RecordID)
+		assert.Equal(t, et, ii[0].Type)
+
 	})
 	t.Run("should create new auction contract from scratch", func(t *testing.T) {
 		// given
@@ -119,8 +114,10 @@ func TestUpdateContractESI(t *testing.T) {
 		startLocation := factory.CreateEveLocationStructure()
 		buyout := 10000000000.01
 		volume := 0.01
-		contractData := []map[string]any{
-			{
+		httpmock.RegisterResponder(
+			"GET",
+			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/", c.ID),
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{{
 				"acceptor_id":           0,
 				"assignee_id":           0,
 				"availability":          "public",
@@ -136,47 +133,36 @@ func TestUpdateContractESI(t *testing.T) {
 				"status":                "outstanding",
 				"type":                  "auction",
 				"volume":                volume,
-			},
-		}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/", c.ID),
-			httpmock.NewJsonResponderOrPanic(200, contractData),
+			}}),
 		)
 		recordID := int64(123456)
 		quantity := 7
 		et := factory.CreateEveType()
-		itemData := []map[string]any{
-			{
+		httpmock.RegisterResponder(
+			"GET",
+			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/%d/items/", c.ID, contractID),
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{{
 				"is_included":  true,
 				"is_singleton": false,
 				"quantity":     quantity,
 				"record_id":    recordID,
 				"type_id":      et.ID,
-			},
-		}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/%d/items/", c.ID, contractID),
-			httpmock.NewJsonResponderOrPanic(200, itemData),
+			}}),
 		)
 		const (
 			bidID  = 123456
 			amount = 123.45
 		)
 		bidder := factory.CreateEveEntityCorporation()
-		bidData := []map[string]any{
-			{
+		httpmock.RegisterResponder(
+			"GET",
+			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/%d/bids/", c.ID, contractID),
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{{
 				"amount":    amount,
 				"bid_id":    bidID,
 				"bidder_id": bidder.ID,
 				"date_bid":  "2017-01-01T10:10:10Z",
-			},
-		}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/%d/bids/", c.ID, contractID),
-			httpmock.NewJsonResponderOrPanic(200, bidData),
+			}}),
 		)
 		// when
 		changed, err := s.updateContractsESI(ctx, app.CorporationSectionUpdateParams{
@@ -184,32 +170,28 @@ func TestUpdateContractESI(t *testing.T) {
 			Section:       app.SectionCorporationContracts,
 		})
 		// then
-		if assert.NoError(t, err) {
-			assert.True(t, changed)
-			o, err := st.GetCorporationContract(ctx, c.ID, contractID)
-			if assert.NoError(t, err) {
-				assert.Equal(t, time.Date(2017, 6, 13, 13, 12, 32, 0, time.UTC), o.DateExpired)
-				assert.Equal(t, time.Date(2017, 6, 5, 13, 12, 32, 0, time.UTC), o.DateIssued)
-				assert.Equal(t, startLocation.ID, o.StartLocation.ID)
-				assert.Equal(t, app.ContractStatusOutstanding, o.Status)
-				assert.Equal(t, app.ContractTypeAuction, o.Type)
-				assert.Equal(t, buyout, o.Buyout)
-			}
-			ii, err := st.ListCorporationContractItems(ctx, o.ID)
-			if assert.NoError(t, err) {
-				assert.Len(t, ii, 1)
-				assert.Equal(t, quantity, ii[0].Quantity)
-				assert.Equal(t, recordID, ii[0].RecordID)
-				assert.Equal(t, et, ii[0].Type)
-			}
-			bb, err := st.ListCorporationContractBids(ctx, o.ID)
-			if assert.NoError(t, err) {
-				assert.Len(t, bb, 1)
-				assert.InDelta(t, amount, bb[0].Amount, 0.1)
-				assert.Equal(t, bidder, bb[0].Bidder)
-				assert.Equal(t, time.Date(2017, 1, 1, 10, 10, 10, 0, time.UTC), bb[0].DateBid)
-			}
-		}
+		require.NoError(t, err)
+		assert.True(t, changed)
+		o, err := st.GetCorporationContract(ctx, c.ID, contractID)
+		require.NoError(t, err)
+		assert.Equal(t, time.Date(2017, 6, 13, 13, 12, 32, 0, time.UTC), o.DateExpired)
+		assert.Equal(t, time.Date(2017, 6, 5, 13, 12, 32, 0, time.UTC), o.DateIssued)
+		assert.Equal(t, startLocation.ID, o.StartLocation.ID)
+		assert.Equal(t, app.ContractStatusOutstanding, o.Status)
+		assert.Equal(t, app.ContractTypeAuction, o.Type)
+		assert.Equal(t, buyout, o.Buyout)
+		ii, err := st.ListCorporationContractItems(ctx, o.ID)
+		require.NoError(t, err)
+		assert.Len(t, ii, 1)
+		assert.Equal(t, quantity, ii[0].Quantity)
+		assert.Equal(t, recordID, ii[0].RecordID)
+		assert.Equal(t, et, ii[0].Type)
+		bb, err := st.ListCorporationContractBids(ctx, o.ID)
+		require.NoError(t, err)
+		assert.Len(t, bb, 1)
+		assert.InDelta(t, amount, bb[0].Amount, 0.1)
+		assert.Equal(t, bidder, bb[0].Bidder)
+		assert.Equal(t, time.Date(2017, 1, 1, 10, 10, 10, 0, time.UTC), bb[0].DateBid)
 	})
 	t.Run("should update existing contract", func(t *testing.T) {
 		// given
@@ -227,8 +209,10 @@ func TestUpdateContractESI(t *testing.T) {
 			Type:          app.ContractTypeCourier,
 		})
 		acceptor := factory.CreateEveEntityCorporation()
-		data := []map[string]any{
-			{
+		httpmock.RegisterResponder(
+			"GET",
+			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/", c.ID),
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{{
 				"acceptor_id":           acceptor.ID,
 				"assignee_id":           0,
 				"availability":          "public",
@@ -247,30 +231,24 @@ func TestUpdateContractESI(t *testing.T) {
 				"status":                "finished",
 				"type":                  "courier",
 				"volume":                o1.Volume,
-			},
-		}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/", c.ID),
-			httpmock.NewJsonResponderOrPanic(200, data))
+			}}),
+		)
 		// when
 		changed, err := s.updateContractsESI(ctx, app.CorporationSectionUpdateParams{
 			CorporationID: c.ID,
 			Section:       app.SectionCorporationContracts,
 		})
 		// then
-		if assert.NoError(t, err) {
-			assert.True(t, changed)
-			o2, err := st.GetCorporationContract(ctx, c.ID, o1.ContractID)
-			if assert.NoError(t, err) {
-				assert.Equal(t, acceptor, o2.Acceptor)
-				assert.Equal(t, app.ContractStatusFinished, o2.Status)
-				assert.Equal(t, time.Date(2017, 6, 6, 13, 12, 32, 0, time.UTC), o2.DateAccepted.MustValue())
-				assert.Equal(t, time.Date(2017, 6, 7, 13, 12, 32, 0, time.UTC), o2.DateCompleted.MustValue())
-				assert.Equal(t, o1.DateIssued, o2.DateIssued)
-				assert.Equal(t, o1.DateExpired, o2.DateExpired)
-			}
-		}
+		require.NoError(t, err)
+		assert.True(t, changed)
+		o2, err := st.GetCorporationContract(ctx, c.ID, o1.ContractID)
+		require.NoError(t, err)
+		assert.Equal(t, acceptor, o2.Acceptor)
+		assert.Equal(t, app.ContractStatusFinished, o2.Status)
+		assert.Equal(t, time.Date(2017, 6, 6, 13, 12, 32, 0, time.UTC), o2.DateAccepted.MustValue())
+		assert.Equal(t, time.Date(2017, 6, 7, 13, 12, 32, 0, time.UTC), o2.DateCompleted.MustValue())
+		assert.Equal(t, o1.DateIssued, o2.DateIssued)
+		assert.Equal(t, o1.DateExpired, o2.DateExpired)
 	})
 	t.Run("should not update unchanged contract", func(t *testing.T) {
 		// given
@@ -287,8 +265,10 @@ func TestUpdateContractESI(t *testing.T) {
 			Status:        app.ContractStatusOutstanding,
 			Type:          app.ContractTypeCourier,
 		})
-		data := []map[string]any{
-			{
+		httpmock.RegisterResponder(
+			"GET",
+			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/", c.ID),
+			httpmock.NewJsonResponderOrPanic(200, []map[string]any{{
 				"availability":          "public",
 				"buyout":                o1.Buyout,
 				"contract_id":           o1.ContractID,
@@ -303,24 +283,18 @@ func TestUpdateContractESI(t *testing.T) {
 				"status":                "outstanding",
 				"type":                  "courier",
 				"volume":                o1.Volume,
-			},
-		}
-		httpmock.RegisterResponder(
-			"GET",
-			fmt.Sprintf("https://esi.evetech.net/v1/corporations/%d/contracts/", c.ID),
-			httpmock.NewJsonResponderOrPanic(200, data))
+			}}),
+		)
 		// when
 		changed, err := s.updateContractsESI(ctx, app.CorporationSectionUpdateParams{
 			CorporationID: c.ID,
 			Section:       app.SectionCorporationContracts,
 		})
 		// then
-		if assert.NoError(t, err) {
-			assert.True(t, changed)
-			o2, err := st.GetCorporationContract(ctx, c.ID, o1.ContractID)
-			if assert.NoError(t, err) {
-				assert.Equal(t, o1.UpdatedAt, o2.UpdatedAt)
-			}
-		}
+		require.NoError(t, err)
+		assert.True(t, changed)
+		o2, err := st.GetCorporationContract(ctx, c.ID, o1.ContractID)
+		require.NoError(t, err)
+		assert.Equal(t, o1.UpdatedAt, o2.UpdatedAt)
 	})
 }
