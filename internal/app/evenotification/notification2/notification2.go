54e350cf857a18628e7e90446679ca887bde7c57
ErikKalkoken <erik.kalkoken@gmail.com>
2026-02-28 12:35:24 +0100

    fix: mercenary den notification structs have wrong tags

--- internal/app/evenotification/notification2/notification2.go
+++ internal/app/evenotification/notification2/notification2.go
@@ -2,30 +2,30 @@
 package notification2
 
 type MercenaryDenAttacked struct {
-	AggressorAllianceName    string  `json:"aggressorAllianceName"`
-	AggressorCharacterID     int64   `json:"aggressorCharacterID"`
-	AggressorCorporationName string  `json:"aggressorCorporationName"`
-	ArmorPercentage          float64 `json:"armorPercentage"`
-	HullPercentage           float64 `json:"hullPercentage"`
-	ItemID                   int64   `json:"itemID"`
-	MercenaryDenShowInfoData []any   `json:"mercenaryDenShowInfoData"`
-	PlanetID                 int64   `json:"planetID"`
-	PlanetShowInfoData       []any   `json:"planetShowInfoData"`
-	ShieldPercentage         float64 `json:"shieldPercentage"`
-	SolarSystemID            int64   `json:"solarsystemID"`
-	TypeID                   int64   `json:"typeID"`
+	AggressorAllianceName    string  `yaml:"aggressorAllianceName"`
+	AggressorCharacterID     int64   `yaml:"aggressorCharacterID"`
+	AggressorCorporationName string  `yaml:"aggressorCorporationName"`
+	ArmorPercentage          float64 `yaml:"armorPercentage"`
+	HullPercentage           float64 `yaml:"hullPercentage"`
+	ItemID                   int64   `yaml:"itemID"`
+	MercenaryDenShowInfoData []any   `yaml:"mercenaryDenShowInfoData"`
+	PlanetID                 int64   `yaml:"planetID"`
+	PlanetShowInfoData       []any   `yaml:"planetShowInfoData"`
+	ShieldPercentage         float64 `yaml:"shieldPercentage"`
+	SolarSystemID            int64   `yaml:"solarsystemID"`
+	TypeID                   int64   `yaml:"typeID"`
 }
 
 type MercenaryDenReinforced struct {
-	AggressorAllianceName    string `json:"aggressorAllianceName"`
-	AggressorCharacterID     int64  `json:"aggressorCharacterID"`
-	AggressorCorporationName string `json:"aggressorCorporationName"`
-	ItemID                   int64  `json:"itemID"`
-	MercenaryDenShowInfoData []any  `json:"mercenaryDenShowInfoData"`
-	PlanetID                 int64  `json:"planetID"`
-	PlanetShowInfoData       []any  `json:"planetShowInfoData"`
-	SolarSystemID            int64  `json:"solarsystemID"`
-	TimestampEntered         int64  `json:"timestampEntered"`
-	TimestampExited          int64  `json:"timestampExited"`
-	TypeID                   int64  `json:"typeID"`
+	AggressorAllianceName    string `yaml:"aggressorAllianceName"`
+	AggressorCharacterID     int64  `yaml:"aggressorCharacterID"`
+	AggressorCorporationName string `yaml:"aggressorCorporationName"`
+	ItemID                   int64  `yaml:"itemID"`
+	MercenaryDenShowInfoData []any  `yaml:"mercenaryDenShowInfoData"`
+	PlanetID                 int64  `yaml:"planetID"`
+	PlanetShowInfoData       []any  `yaml:"planetShowInfoData"`
+	SolarSystemID            int64  `yaml:"solarsystemID"`
+	TimestampEntered         int64  `yaml:"timestampEntered"`
+	TimestampExited          int64  `yaml:"timestampExited"`
+	TypeID                   int64  `yaml:"typeID"`
 }
