bf6f9ed2608185e744cd4d68b6cce8b204f805c9
ErikKalkoken <erik.kalkoken@gmail.com>
2026-03-05 16:55:13 +0100

    refactor: split infowindow package files

--- internal/app/infowindow/infowindow_internal_test.go
+++ internal/app/infowindow/infowindow_internal_test.go
@@ -1,45 +1,112 @@
 package infowindow
 
-// import (
-// 	"context"
-// 	"testing"
+import (
+	"net/http"
+	"strings"
+	"testing"
 
-// 	"fyne.io/fyne/v2/test"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/test"
 
-// 	"github.com/ErikKalkoken/evebuddy/internal/app/testutil"
-// )
+	"github.com/fnt-eve/goesi-openapi"
 
-// func TestInfoWindow_CanRenderLocationInfo(t *testing.T) {
-// 	test.ApplyTheme(t, test.Theme())
-// 	db, st, factory := testutil.NewDBOnDisk(t)
-// 	defer db.Close()
-// 	u := MakeFakeBaseUI(st, test.NewTempApp(t), true)
-// 	ctx := context.Background()
-// 	makeInfoWindow := func() *InfoWindow {
-// 		return NewInfoWindow(InfoWindowParams{
-// 			cs:       u.cs,
-// 			eis:      u.eis,
-// 			eus:      u.eus,
-// 			isMobile: u.isMobile,
-// 			js:       u.js,
-// 			settings: u.settings,
-// 			scs:      u.scs,
-// 			u:        u,
-// 			w:        u.MainWindow(),
-// 		})
-// 	}
-// 	t.Run("can render full location", func(t *testing.T) {
-// 		l := factory.CreateEveLocationStation()
-// 		iw := makeInfoWindow()
-// 		a := newLocationInfo(iw, l.ID)
-// 		a.update(ctx)
-// 		test.RenderObjectToMarkup(a)
-// 	})
-// 	t.Run("can render minimal location", func(t *testing.T) {
-// 		l := factory.CreateEveLocationEmptyStructure()
-// 		iw := makeInfoWindow()
-// 		a := newLocationInfo(iw, l.ID)
-// 		a.update(ctx)
-// 		test.RenderObjectToMarkup(a)
-// 	})
-// }
+	"github.com/ErikKalkoken/evebuddy/internal/app/characterservice"
+	"github.com/ErikKalkoken/evebuddy/internal/app/eveuniverseservice"
+	"github.com/ErikKalkoken/evebuddy/internal/app/icons"
+	"github.com/ErikKalkoken/evebuddy/internal/app/statuscacheservice"
+	"github.com/ErikKalkoken/evebuddy/internal/app/testutil"
+	"github.com/ErikKalkoken/evebuddy/internal/janiceservice"
+)
+
+type UIServiceFake struct {
+	app fyne.App
+}
+
+func (u *UIServiceFake) GetOrCreateWindow(id string, titles ...string) (window fyne.Window, created bool) {
+	return u.app.NewWindow("Dunmy"), true
+}
+
+func (u *UIServiceFake) HumanizeError(err error) string {
+	return err.Error()
+}
+
+func (u *UIServiceFake) IsDeveloperMode() bool {
+	return false
+}
+
+func (u *UIServiceFake) IsOffline() bool {
+	return false
+}
+
+func (u *UIServiceFake) MakeWindowTitle(parts ...string) string {
+	return strings.Join(parts, " - ")
+}
+
+func (u *UIServiceFake) ShowInformationDialog(title, message string, parent fyne.Window) {}
+
+type SettingsFake struct{}
+
+func (s *SettingsFake) PreferMarketTab() bool {
+	return false
+}
+
+func TestInfoWindow_CanRenderLocationInfo(t *testing.T) {
+	test.ApplyTheme(t, test.Theme())
+	db, st, factory := testutil.NewDBOnDisk(t)
+	defer db.Close()
+
+	esiClient := goesi.NewESIClientWithOptions(http.DefaultClient, goesi.ClientOptions{
+		UserAgent: "EveBuddy/1.0 (test@kalkoken.net)",
+	})
+	scs := statuscacheservice.New(st)
+	if err := scs.InitCache(t.Context()); err != nil {
+		panic(err)
+	}
+	eus := eveuniverseservice.New(eveuniverseservice.Params{
+		ESIClient:          esiClient,
+		StatusCacheService: scs,
+		Storage:            st,
+	})
+	cs := characterservice.New(characterservice.Params{
+		Cache:              testutil.NewCacheFake2(),
+		ESIClient:          esiClient,
+		EveUniverseService: eus,
+		StatusCacheService: scs,
+		Storage:            st,
+	})
+	app := test.NewTempApp(t)
+	makeInfoWindow := func() *InfoWindow {
+		return New(Params{
+			CharacterService: cs,
+			EveImageService: &testutil.EveImageServiceFake{
+				Character:   icons.Characterplaceholder64Jpeg,
+				Alliance:    icons.Corporationplaceholder64Png,
+				Corporation: icons.Corporationplaceholder64Png,
+				Err:         nil,
+				Faction:     icons.Factionplaceholder64Png,
+				Type:        icons.Typeplaceholder64Png,
+			},
+			EveUniverseService: eus,
+			IsMobile:           false,
+			JaniceService:      new(janiceservice.JaniceService),
+			StatusCacheService: scs,
+			Settings:           new(SettingsFake),
+			UIService:          new(UIServiceFake),
+			Window:             app.NewWindow("Dummy"),
+		})
+	}
+	t.Run("can render full location", func(t *testing.T) {
+		l := factory.CreateEveLocationStation()
+		iw := makeInfoWindow()
+		a := newLocationInfo(iw, l.ID)
+		a.update(t.Context())
+		test.RenderObjectToMarkup(a)
+	})
+	t.Run("can render minimal location", func(t *testing.T) {
+		l := factory.CreateEveLocationEmptyStructure()
+		iw := makeInfoWindow()
+		a := newLocationInfo(iw, l.ID)
+		a.update(t.Context())
+		test.RenderObjectToMarkup(a)
+	})
+}
---
+++ internal/app/infowindow/regioninfo.go
@@ -0,0 +1,116 @@
+package infowindow
+
+import (
+	"context"
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/ErikKalkoken/evebuddy/internal/app/icons"
+	"github.com/ErikKalkoken/evebuddy/internal/xslices"
+)
+
+type regionInfo struct {
+	widget.BaseWidget
+	baseInfo
+
+	description    *widget.Label
+	constellations *entityList
+	id             int64
+	logo           *canvas.Image
+	tabs           *container.AppTabs
+}
+
+func newRegionInfo(iw *InfoWindow, id int64) *regionInfo {
+	a := &regionInfo{
+		id:          id,
+		description: newLabelWithWrapAndSelectable(""),
+		logo:        makeInfoLogo(),
+		tabs:        container.NewAppTabs(),
+	}
+	a.logo.Resource = icons.Region64Png
+	a.initBase(iw)
+	a.ExtendBaseWidget(a)
+	a.constellations = newEntityList(a.iw.show)
+	constellations := container.NewTabItem("Constellations", a.constellations)
+	a.tabs = container.NewAppTabs(
+		container.NewTabItem("Description", container.NewVScroll(a.description)),
+		constellations,
+	)
+	a.tabs.Select(constellations)
+	return a
+}
+
+func (a *regionInfo) CreateRenderer() fyne.WidgetRenderer {
+	p := theme.Padding()
+	main := container.NewVBox(
+		container.New(layout.NewCustomPaddedVBoxLayout(-2*p),
+			a.name,
+			widget.NewLabel("Region"),
+		),
+	)
+	top := container.NewBorder(
+		nil,
+		nil,
+		container.NewVBox(
+			container.NewPadded(a.logo),
+			container.New(
+				layout.NewCustomPaddedHBoxLayout(3*p),
+				layout.NewSpacer(),
+				a.iw.makeZKillboardIcon(a.id, infoRegion),
+				a.iw.makeDotlanIcon(a.id, infoRegion),
+				layout.NewSpacer(),
+			),
+		),
+		nil,
+		main,
+	)
+	c := container.NewBorder(top, nil, nil, nil, a.tabs)
+	return widget.NewSimpleRenderer(c)
+}
+
+func (a *regionInfo) update(ctx context.Context) error {
+	o, err := a.iw.eus.GetOrCreateRegionESI(ctx, a.id)
+	if err != nil {
+		return err
+	}
+	g := new(errgroup.Group)
+	g.Go(func() error {
+		if !a.iw.u.IsDeveloperMode() {
+			x := newAttributeItem("EVE ID", fmt.Sprint(o.ID))
+			x.Action = func(v any) {
+				fyne.CurrentApp().Clipboard().SetContent(v.(string))
+			}
+			attributeList := newAttributeList(a.iw, []attributeItem{x}...)
+			attributesTab := container.NewTabItem("Attributes", attributeList)
+			fyne.Do(func() {
+				a.tabs.Append(attributesTab)
+			})
+		}
+		fyne.Do(func() {
+			a.name.SetText(o.Name)
+			a.description.SetText(o.DescriptionPlain())
+			a.tabs.Refresh()
+		})
+		return nil
+	})
+	g.Go(func() error {
+		oo, err := a.iw.eus.GetRegionConstellationsESI(ctx, o.ID)
+		if err != nil {
+			return err
+		}
+		items := xslices.Map(oo, newEntityItemFromEveEntity)
+		fyne.Do(func() {
+			a.constellations.set(items...)
+			a.tabs.Refresh()
+		})
+		return nil
+	})
+	return nil
+}
