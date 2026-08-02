5a7d0765b7c645dbe58f588b31a7cd186cff0b8d
ErikKalkoken <erik.kalkoken@gmail.com>
2026-08-02 17:05:19 +0200

    fix: faction not shown for character in info window

--- internal/app/ui/infoviewer/characterinfo.go
+++ internal/app/ui/infoviewer/characterinfo.go
@@ -272,7 +272,7 @@ func (a *characterInfo) makeAttributes(ctx context.Context, o *app.EveCharacter,
 	if v, ok := o.Alliance.Value(); ok {
 		attributes = append(attributes, newAttributeItem("Alliance", v))
 	}
-	if v, ok := o.Alliance.Value(); ok {
+	if v, ok := o.Faction.Value(); ok {
 		attributes = append(attributes, newAttributeItem("Faction", v))
 	}
 	var u any
