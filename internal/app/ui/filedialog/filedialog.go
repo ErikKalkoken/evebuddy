979eacb83a307ea5937566c49a5e92c196c6d2b3
ErikKalkoken <erik.kalkoken@gmail.com>
2026-09-05 22:33:39 +0200

    feat: add ShowOpen to filedialog package

--- internal/app/ui/filedialog/filedialog.go
+++ internal/app/ui/filedialog/filedialog.go
@@ -38,9 +38,33 @@ func ShowSave(u baseUI, arg ShowFileSaveWindowParams) {
 	}
 
 	w, _ := u.GetOrCreateWindow(arg.WindowID, arg.Title)
-
 	d := createFileSaveDialog(u, arg, w)
+	showDialogInWindow(u, w, d)
+}
 
+type ShowFileOpenWindowParams struct {
+	CompletionText string
+	Extensions     []string
+	ShowSnackbar   func(string)
+	Title          string
+	WindowID       string // optional
+	ReadFunc       func(ctx context.Context, r io.Reader) error
+	Window         fyne.Window // required when running on mobile; used as the native picker's parent
+}
+
+func ShowOpen(u baseUI, arg ShowFileOpenWindowParams) {
+	if u.IsMobile() {
+		d := createFileOpenDialog(u, arg, arg.Window)
+		d.Show()
+		return
+	}
+
+	w, _ := u.GetOrCreateWindow(arg.WindowID, arg.Title)
+	d := createFileOpenDialog(u, arg, w)
+	showDialogInWindow(u, w, d)
+}
+
+func showDialogInWindow(u baseUI, w fyne.Window, d *dialog.FileDialog) {
 	_, s := u.MainWindow().Canvas().InteractiveArea()
 	winSize := fyne.NewSize(s.Width*0.8, s.Height*0.8)
 	d.SetOnClosed(func() {
@@ -78,6 +102,34 @@ func createFileSaveDialog(u baseUI, arg ShowFileSaveWindowParams, w fyne.Window)
 	if arg.Filename != "" {
 		d.SetFileName(arg.Filename)
 	}
+	if len(arg.Extensions) > 0 {
+		d.SetFilter(storage.NewExtensionFileFilter(arg.Extensions))
+	}
+	return d
+}
+
+func createFileOpenDialog(u baseUI, arg ShowFileOpenWindowParams, w fyne.Window) *dialog.FileDialog {
+	d := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
+		if err != nil {
+			arg.ShowSnackbar("Error: " + u.ErrorDisplay(err))
+			return
+		}
+		if reader == nil {
+			return
+		}
+		go func() {
+			defer reader.Close()
+			err := arg.ReadFunc(context.Background(), reader)
+			if err != nil {
+				slog.Error(arg.Title, "error", err)
+				fyne.Do(func() {
+					arg.ShowSnackbar("Error: " + u.ErrorDisplay(err))
+				})
+				return
+			}
+			fyne.Do(func() { arg.ShowSnackbar(arg.CompletionText) })
+		}()
+	}, w)
 	if len(arg.Extensions) > 0 {
 		d.SetFilter(storage.NewExtensionFileFilter(arg.Extensions))
 	}
