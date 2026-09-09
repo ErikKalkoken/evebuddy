b8d25dd848c9ed6f128a6bb145f25045f6683e7f
ErikKalkoken <erik.kalkoken@gmail.com>
2026-09-09 15:39:37 +0200

    test: add coverage for corporation.go

--- internal/app/corporation_test.go
+++ internal/app/corporation_test.go
@@ -7,9 +7,28 @@ import (
 	"github.com/ErikKalkoken/evebuddy/internal/xassert"
 )
 
+func TestDivisionID(t *testing.T) {
+	xassert.Equal(t, int64(1), app.Division1.ID())
+}
+
+func TestDivisionDefaultWalletName(t *testing.T) {
+	xassert.Equal(t, "Master Wallet", app.Division1.DefaultWalletName())
+	xassert.Equal(t, "", app.DivisionZero.DefaultWalletName())
+}
+
+func TestCorporationWalletJournalEntryRefTypeDisplay(t *testing.T) {
+	we := app.CorporationWalletJournalEntry{RefType: "market_transaction"}
+	xassert.Equal(t, "Market Transaction", we.RefTypeDisplay())
+}
+
+func TestCorporationWalletTransactionTotal(t *testing.T) {
+	wt := &app.CorporationWalletTransaction{UnitPrice: 1.5, Quantity: 4}
+	xassert.Equal(t, 6.0, wt.Total())
+}
+
 func TestCorporation_IDorZero(t *testing.T) {
 	t.Run("nil", func(t *testing.T) {
-		c := new(app.Corporation)
+		var c *app.Corporation
 		xassert.Equal(t, 0, c.IDOrZero())
 	})
 	t.Run("not nil", func(t *testing.T) {
@@ -20,7 +39,7 @@ func TestCorporation_IDorZero(t *testing.T) {
 
 func TestCorporation_NameorZero(t *testing.T) {
 	t.Run("corp is nil", func(t *testing.T) {
-		c := new(app.Corporation)
+		var c *app.Corporation
 		xassert.Equal(t, "", c.NameOrZero())
 	})
 	t.Run("corp is not nil", func(t *testing.T) {
